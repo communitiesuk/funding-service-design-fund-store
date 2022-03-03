62911842559d6519526ffbaedac2436fc26589b3
jharrington <jack.harrington@evoco.co.uk>
2022-03-03 15:09:55 +0000

    POST to search now takes url arg

--- apis/fund_discovery_api/fund_discovery_search.py
+++ apis/fund_discovery_api/fund_discovery_search.py
@@ -1,14 +1,13 @@
 import warnings
 
 
-def search(to_search: dict, words):
+def search(to_search: dict, word_list):
     warnings.warn(
         "Search function used in the fund store shouldn't be used in"
         " production",
         DeprecationWarning,
     )
     return_list = []
-    word_list = words.split("-")
     for fund_data in to_search:
         for word in word_list:
             for data_point in fund_data:
