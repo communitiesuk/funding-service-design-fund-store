f848e93fa8be500e7fa1394a0ceb5f59cb07288f
jharrington <jack.harrington@evoco.co.uk>
2022-03-03 15:09:55 +0000

    POST to search now takes url arg

--- apis/fund_discovery_api/fund_discovery_routes.py
+++ apis/fund_discovery_api/fund_discovery_routes.py
@@ -6,14 +6,19 @@ from apis.fund_discovery_api.fund_discovery_models import api
 from apis.fund_discovery_api.fund_discovery_models import full_search_result
 from apis.fund_discovery_api.fund_discovery_search import search
 from apis.fund_store_api.fund_store_dummy_data import FUNDS
+from flask_restx import reqparse
 from flask_restx import Resource
 
+search_parser = reqparse.RequestParser()
+search_parser.add_argument("search_items", action="split")
 
-@api.route("/<words>")
-@api.param("words", "A series of hyphon seperated words.")
+
+@api.route("/")
 class FundSearch(Resource):
     @api.doc("search_funds")
     @api.marshal_list_with(full_search_result)
-    def post(self, words):
+    @api.expect(search_parser)
+    def post(self):
         """List all funds"""
-        return search(FUNDS.get_all(), words)
+        args = search_parser.parse_args()
+        return search(FUNDS.get_all(), args["search_items"])
