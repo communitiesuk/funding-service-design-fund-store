95a2d6f8f7e108379aa8e9c6be01af5079712818
jharrington <jack.harrington@evoco.co.uk>
2022-03-03 15:21:27 +0000

    Search works as intended.

--- apis/fund_discovery_api/fund_discovery_search.py
+++ apis/fund_discovery_api/fund_discovery_search.py
@@ -9,9 +9,14 @@ def search(to_search: dict, word_list):
     )
     return_list = []
     for fund_data in to_search:
-        for word in word_list:
-            for data_point in fund_data:
+        word_found = False
+        for data_point in fund_data:
+            for word in word_list:
                 if word in fund_data[data_point]:
                     return_list.append(fund_data)
+                    word_found = True
                     break
+            if word_found:
+                break
+
     return return_list
