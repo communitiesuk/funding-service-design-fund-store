344f78bed05169089eec9edfdfab2c2ddb5ece6e
jharrington <jack.harrington@evoco.co.uk>
2022-03-07 11:11:13 +0000

    Test data is now independant from (future) production data.

--- apis/fund_discovery_api/fund_discovery_routes.py
+++ apis/fund_discovery_api/fund_discovery_routes.py
@@ -5,20 +5,20 @@ in it.
 from apis.fund_discovery_api.fund_discovery_models import api
 from apis.fund_discovery_api.fund_discovery_models import full_search_result
 from apis.fund_discovery_api.fund_discovery_search import search
-from apis.fund_store_api.fund_store_dummy_data import FUNDS
 from flask_restx import reqparse
 from flask_restx import Resource
 
-search_parser = reqparse.RequestParser()
-search_parser.add_argument("search_items", action="split")
 
+def init_routes(dummy_dao):
+    search_parser = reqparse.RequestParser()
+    search_parser.add_argument("search_items", action="split")
 
-@api.route("/")
-class FundSearch(Resource):
-    @api.doc("search_funds")
-    @api.marshal_list_with(full_search_result)
-    @api.expect(search_parser)
-    def post(self):
-        """List all funds"""
-        args = search_parser.parse_args()
-        return search(FUNDS.get_all(), args["search_items"])
+    @api.route("/")
+    class FundSearch(Resource):
+        @api.doc("search_funds")
+        @api.marshal_list_with(full_search_result)
+        @api.expect(search_parser)
+        def post(self):
+            """List all funds"""
+            args = search_parser.parse_args()
+            return search(dummy_dao.get_all(), args["search_items"])
--- apis/fund_store_api/fund_api_routes.py
+++ apis/fund_store_api/fund_api_routes.py
@@ -4,25 +4,24 @@ in it.
 """
 from apis.fund_store_api.fund_api_models import api
 from apis.fund_store_api.fund_api_models import full_fund_model
-from apis.fund_store_api.fund_store_dummy_data import FUNDS
 from flask_restx import Resource
 
 
-@api.route("/")
-class FundList(Resource):
-    @api.doc("list_funds")
-    @api.marshal_list_with(full_fund_model)
-    def get(self):
-        """List all funds"""
-        return FUNDS.get_all()
+def init_routes(dummy_dao):
+    @api.route("/")
+    class FundList(Resource):
+        @api.doc("list_funds")
+        @api.marshal_list_with(full_fund_model)
+        def get(self):
+            """List all funds"""
+            return dummy_dao.get_all()
 
-
-@api.route("/<fund_identifier>")
-@api.param("fund_identifier", "The id of the fund")
-@api.response(404, "Fund not found")
-class Fund(Resource):
-    @api.doc("get_fund")
-    @api.marshal_with(full_fund_model)
-    def get(self, fund_identifier):
-        """Fetch a fund given its name"""
-        return FUNDS.get(fund_identifier)
+    @api.route("/<fund_identifier>")
+    @api.param("fund_identifier", "The id of the fund")
+    @api.response(404, "Fund not found")
+    class Fund(Resource):
+        @api.doc("get_fund")
+        @api.marshal_with(full_fund_model)
+        def get(self, fund_identifier):
+            """Fetch a fund given its name"""
+            return dummy_dao.get(fund_identifier)
