622f08c8154f470ada4459cfd0e65158b8368296
J <65361824+SentientHousePlant@users.noreply.github.com>
2022-05-26 17:04:13 +0100

    🏷 Added type hints

--- core/funds.py
+++ core/funds.py
@@ -1,5 +1,10 @@
-def get_funds():
-    return "Worked!"
+from ast import List
 
-def get_fund(fund_id):
+
+from typing import List
+
+def get_funds(query : List[str]):
+    return f"Worked! You searched for {query}"
+
+def get_fund(fund_id : str):
     return f"You gave {fund_id}"
--- core/rounds.py
+++ core/rounds.py
@@ -1,5 +1,5 @@
-def get_rounds_for_fund(fund_id):
+def get_rounds_for_fund(fund_id : str):
     return f"Worked, here's the rounds for this fund with id: {fund_id}"
 
-def get_round(fund_id, round_id):
+def get_round(fund_id : str, round_id : str):
     return f"here's your round with id: {round_id} from fund id: {fund_id}"
