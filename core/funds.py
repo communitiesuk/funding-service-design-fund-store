ffe6663a61cb875528f588754ae2e046d887bb98
Gio Karunakaran <gio.karunakaran@version1.com>
2022-05-27 17:42:02 +0100

    adding tags, README and fixing funds endpoint

--- core/funds.py
+++ core/funds.py
@@ -3,7 +3,10 @@ from ast import List
 
 from typing import List
 
-def get_funds(query : List[str]):
+from flask import request
+
+def get_funds():
+    query = request.args.get('query')
     return f"Worked! You searched for {query}"
 
 def get_fund(fund_id : str):
