c75ab218a5c9cc8be074c1a8be12f2e50c541eaf
Gio Karunakaran <gio.karunakaran@version1.com>
2022-06-01 15:52:34 +0100

    python methods for returning rounds

--- core/rounds.py
+++ core/rounds.py
@@ -1,5 +1,20 @@
-def get_rounds_for_fund(fund_id : str):
-    return f"Worked, here's the rounds for this fund with id: {fund_id}"
+from connexion import NoContent
+from core.data_operations import round_data
 
-def get_round(fund_id : str, round_id : str):
-    return f"here's your round with id: {round_id} from fund id: {fund_id}"
+# from flask import request
+
+
+def get_rounds_for_fund(fund_id: str):
+    rounds = round_data.ROUNDS_DUMMY_DAO.get_all(fund_id)
+    if len(rounds) > 0:
+        return rounds
+    else:
+        return NoContent, 404
+
+
+def get_round(fund_id: str, round_id: str):
+    round = round_data.ROUNDS_DUMMY_DAO.get_one(fund_id, round_id)
+    if round:
+        return round
+    else:
+        return NoContent, 404
