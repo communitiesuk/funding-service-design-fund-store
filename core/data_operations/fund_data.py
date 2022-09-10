9df34d5c6e894e6fb465dd496c7bba3e74cfe628
Steve Walton <steve.walton@evoco.co.uk>
2022-09-10 22:55:08 +0100

    Removing email address from fund object

--- core/data_operations/fund_data.py
+++ core/data_operations/fund_data.py
@@ -8,7 +8,6 @@ FUND_DATA = [
     {
         "id": "47aef2f5-3fcb-4d45-acb5-f0152b5f03c4",
         "name": "Community Ownership Fund",
-        "contact_email": "COF@levellingup.gov.uk",
         "description": (
             "The Community Ownership Fund is a £150 million fund over 4 years to support community groups across "
             "England, Wales, Scotland and Northern Ireland to take ownership of assets which are at risk of being "
