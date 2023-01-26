1bc47ed31e0f90963da8942d35e8f67eb211780a
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-01-26 10:38:33 +0000

    ran pre-commit

--- core/data_operations/round_data.py
+++ core/data_operations/round_data.py
@@ -1,13 +1,13 @@
 """
     Round configuration
 """
+import os
+
 from config import Config
 from core.dummy_dao import RoundDAO
-import os
 
 shared_cof_r2_data = {
     "fund_id": "47aef2f5-3fcb-4d45-acb5-f0152b5f03c4",
-
     "contact_details": {
         "phone": "",
         "email_address": "COF@levellingup.gov.uk",
@@ -17,11 +17,9 @@ shared_cof_r2_data = {
         "time": "9am to 5pm",
         "days": "Monday to Friday",
         "closed": (
-            "Easter Sunday, Christmas Day, Boxing Day and New"
-            " Year’s Day"
+            "Easter Sunday, Christmas Day, Boxing Day and New Year’s Day"
         ),
     },
-
     "assessment_criteria_weighting": [
         {
             "id": "e2fd30d2-9207-421c-b8b3-c961bcee138b",
@@ -46,48 +44,54 @@ shared_cof_r2_data = {
     ],
 }
 
+
 def override_fields_from_env(round_data: dict, overridable_fields: list):
     for field in overridable_fields:
-        round_data[field] = os.getenv(f"force_{field}_{round_data['short_name']}", round_data[field])
+        round_data[field] = os.getenv(
+            f"force_{field}_{round_data['short_name']}", round_data[field]
+        )
     return round_data
+
 
 def get_round_data(language):
 
-    overridable_fields = [
-        "opens",
-        "deadline",
-        "assessment_deadline"
-    ]
+    overridable_fields = ["opens", "deadline", "assessment_deadline"]
 
     return [
-        override_fields_from_env({
-            "id": "c603d114-5364-4474-a0c4-c41cbf4d3bbd",
-            "title": "Cylch 2 Window 2"
-            if language == "cy"
-            else "Round 2 Window 2",
-            "short_name": "R2W2",
-            "opens": "2022-10-04 12:00:00",
-            "deadline": "2022-12-14 11:59:00"
-            if not Config.FORCE_OPEN
-            else "2024-12-31 11:59:00",
-            "assessment_deadline": "2023-03-30 12:00:00",
-            **shared_cof_r2_data
-        }, overridable_fields),
-        override_fields_from_env({
-            "id": "5cf439bf-ef6f-431e-92c5-a1d90a4dd32f",
-            "title": "Cylch 2 Window 3"
-            if language == "cy"
-            else "Round 2 Window 3",
-            "short_name": "R2W3",
-            "opens": "2023-02-08 12:00:00"
-            if not Config.FORCE_OPEN
-            else "2022-01-01 12:00:00",
-            "deadline": "2023-04-05 11:59:00"
-            if not Config.FORCE_OPEN
-            else "2024-12-31 11:59:00",
-            "assessment_deadline": "2023-05-05 12:00:00",
-            **shared_cof_r2_data
-        }, overridable_fields),
+        override_fields_from_env(
+            {
+                "id": "c603d114-5364-4474-a0c4-c41cbf4d3bbd",
+                "title": "Cylch 2 Window 2"
+                if language == "cy"
+                else "Round 2 Window 2",
+                "short_name": "R2W2",
+                "opens": "2022-10-04 12:00:00",
+                "deadline": "2022-12-14 11:59:00"
+                if not Config.FORCE_OPEN
+                else "2024-12-31 11:59:00",
+                "assessment_deadline": "2023-03-30 12:00:00",
+                **shared_cof_r2_data,
+            },
+            overridable_fields,
+        ),
+        override_fields_from_env(
+            {
+                "id": "5cf439bf-ef6f-431e-92c5-a1d90a4dd32f",
+                "title": "Cylch 2 Window 3"
+                if language == "cy"
+                else "Round 2 Window 3",
+                "short_name": "R2W3",
+                "opens": "2023-02-08 12:00:00"
+                if not Config.FORCE_OPEN
+                else "2022-01-01 12:00:00",
+                "deadline": "2023-04-05 11:59:00"
+                if not Config.FORCE_OPEN
+                else "2024-12-31 11:59:00",
+                "assessment_deadline": "2023-05-05 12:00:00",
+                **shared_cof_r2_data,
+            },
+            overridable_fields,
+        ),
     ]
 
 
--- tests/test_cof_data.py
+++ tests/test_cof_data.py
@@ -1,19 +1,19 @@
-from core.rounds import get_round, get_rounds_for_fund
+from core.data_operations.round_data import get_round_data
+from core.data_operations.round_data import override_fields_from_env
+from core.rounds import get_round
+from core.rounds import get_rounds_for_fund
 from fsd_utils.config.commonconfig import CommonConfig
-from core.data_operations.round_data import override_fields_from_env, get_round_data
+
 
 class MockRequest_cy:
 
-    args = {
-        "language": "cy"
-    }
+    args = {"language": "cy"}
 
 
 class MockRequest_en:
 
-    args = {
-        "language": "en"
-    }
+    args = {"language": "en"}
+
 
 def test_get_cof_r2w3(mocker, monkeypatch):
 
@@ -21,7 +21,9 @@ def test_get_cof_r2w3(mocker, monkeypatch):
         "core.rounds.request",
         MockRequest_en(),
     )
-    result = get_round(CommonConfig.COF_FUND_ID, CommonConfig.COF_ROUND_2_W3_ID)
+    result = get_round(
+        CommonConfig.COF_FUND_ID, CommonConfig.COF_ROUND_2_W3_ID
+    )
     assert "Round 2 Window 3" == result[0]["title"]
     assert "Monday to Friday" == result[0]["support_availability"]["days"]
 
@@ -32,9 +34,12 @@ def test_get_cof_r2w3_welsh(mocker, monkeypatch):
         "core.rounds.request",
         MockRequest_cy(),
     )
-    result = get_round(CommonConfig.COF_FUND_ID, CommonConfig.COF_ROUND_2_W3_ID)
+    result = get_round(
+        CommonConfig.COF_FUND_ID, CommonConfig.COF_ROUND_2_W3_ID
+    )
     assert "Cylch 2 Window 3" == result[0]["title"]
     assert "Monday to Friday" == result[0]["support_availability"]["days"]
+
 
 def test_get_cof_r2w2(mocker, monkeypatch):
 
@@ -58,12 +63,13 @@ def test_get_cof(mocker, monkeypatch):
     assert "Round 2 Window 3" == result[0][1]["title"]
     assert "Round 2 Window 2" == result[0][0]["title"]
 
+
 def test_override_fields(monkeypatch):
     overridable_fields = ["opens"]
     round_data = {
         "short_name": "a1b2",
         "opens": "abc",
-        "dont_override": "original"
+        "dont_override": "original",
     }
     monkeypatch.setenv("force_opens_a1b2", "changed")
     result = override_fields_from_env(round_data, overridable_fields)
@@ -72,14 +78,21 @@ def test_override_fields(monkeypatch):
 
 
 def test_force_open(monkeypatch):
-    monkeypatch.setattr("core.data_operations.round_data.Config.FORCE_OPEN", "True")
+    monkeypatch.setattr(
+        "core.data_operations.round_data.Config.FORCE_OPEN", "True"
+    )
     result = get_round_data("en")
-    assert "2022-01-01 12:00:00" == result[1]["opens"], "opens field not changed"
+    assert (
+        "2022-01-01 12:00:00" == result[1]["opens"]
+    ), "opens field not changed"
     assert "R2W3" == result[1]["short_name"], "other field changed"
+
 
 def test_override_fields_with_force_open(monkeypatch):
     monkeypatch.setenv("force_opens_R2W3", "changed")
-    monkeypatch.setattr("core.data_operations.round_data.Config.FORCE_OPEN", "True")
+    monkeypatch.setattr(
+        "core.data_operations.round_data.Config.FORCE_OPEN", "True"
+    )
     result = get_round_data("en")
     assert "changed" == result[1]["opens"], "opens field not changed"
     assert "R2W3" == result[1]["short_name"], "other field changed"
