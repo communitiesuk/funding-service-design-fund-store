4c103c87b85225a2ae698ccda0bdc1a7ea07d97e
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-04-27 18:38:20 +0100

    added field and link table

---
+++ db/cof_sql/assessment_fields.sql
@@ -0,0 +1,6 @@
+INSERT INTO assessment_field(id, title, field_type, display_type)
+VALUES('YdtlQZ', 'Organisation name', 'TextField', 'text');
+INSERT INTO assessment_field(id, title, field_type, display_type)
+VALUES('iBCGxY', 'Does your organisation use any other names', 'YesNoField', 'text');
+INSERT INTO assessment_field(id, title, field_type, display_type)
+VALUES('JCACTy', 'Have you done any fundraising in the community', 'YesNoField', 'text');
--- db/models/section.py
+++ db/models/section.py
@@ -1,12 +1,42 @@
 from db import db
+from db import metadata
 from flask_sqlalchemy import DefaultMeta
 from sqlalchemy import Column
 from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
+from sqlalchemy import Table
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 from sqlalchemy_utils import LtreeType
 
+
 BaseModel: DefaultMeta = db.Model
+
+
+section_field_table = Table(
+    "section_field",
+    metadata,
+    Column("section_id", ForeignKey("section.id"), primary_key=True),
+    Column("field_id", ForeignKey("assessment_field.id"), primary_key=True),
+    Column("display_order", Integer, nullable=False, unique=False),
+)
+
+
+class AssessmentField(BaseModel):
+    id = Column(db.String, primary_key=True, nullable=False, unique=True)
+    field_type = Column(
+        "field_type",
+        db.String(),
+        nullable=False,
+        unique=False,
+    )
+    display_type = Column(
+        "display_type", db.String(), nullable=False, unique=False
+    )
+    title = Column("title", db.String(), nullable=False, unique=False)
+    sections = relationship(
+        "Section", secondary=section_field_table, back_populates="fields"
+    )
 
 
 class Section(BaseModel):
@@ -27,3 +57,8 @@ class Section(BaseModel):
         nullable=True,
     )
     path = Column(LtreeType, nullable=False)
+    fields = relationship(
+        "AssesmentField",
+        secondary=section_field_table,
+        back_populates="sections",
+    )
