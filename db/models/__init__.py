c9ab4843fed0bb9515a6f6bb97ab29924d0fdb18
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-05-04 17:11:30 +0100

    clean migrations without translations

--- db/models/__init__.py
+++ db/models/__init__.py
@@ -3,7 +3,8 @@ from .round import Round  # noqa
 from .section import AssessmentField  # noqa
 from .section import Section  # noqa
 from .section import SectionField  # noqa
-from .translations import Translation  # noqa
+
+# from .translations import Translation  # noqa
 
 # from .section import section_field_table  # noqa
 
