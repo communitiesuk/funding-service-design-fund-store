f31dfc3c9633b045b8026c3ee39e0b3f2d205748
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-05-09 16:06:04 +0100

    inserting form name data from script

--- scripts/deprecated_config/sort_application_sections.py
+++ scripts/deprecated_config/sort_application_sections.py
@@ -6,28 +6,46 @@ application_display_config = CommonConfig.COF_R2_ORDERED_FORMS_CONFIG
 def alpha_numeric_sort_section(index, section_config, tree_base_path):
     an_sorted_sections = []
     top_section_tree_level = f"{tree_base_path}.{index+1}"
-    an_sorted_sections.append({
-        "section_name": str(section_config["section_title"]["en"]),
-        "tree_path": str(top_section_tree_level)
-    })
+    an_sorted_sections.append(
+        {
+            "section_name": str(section_config["section_title"]["en"]),
+            "tree_path": str(top_section_tree_level),
+        }
+    )
 
-    for index, form_section in enumerate(section_config["ordered_form_names_within_section"]):
-        current_form_section_tree_level = index+1
-        an_sorted_sections.append({
-            "section_name": str(form_section["en"]).replace('-', ' ').title(),
-            "tree_path": str(f"{top_section_tree_level}.{current_form_section_tree_level}")
-        })
+    for index, form_section in enumerate(
+        section_config["ordered_form_names_within_section"]
+    ):
+        current_form_section_tree_level = index + 1
+        an_sorted_sections.append(
+            {
+                "section_name": str(form_section["en"])
+                .replace("-", " ")
+                .title(),
+                "tree_path": str(
+                    f"{top_section_tree_level}"
+                    f".{current_form_section_tree_level}"
+                ),
+                "form_name": form_section["en"],
+            }
+        )
     return an_sorted_sections
 
 
 def sort_sections_from_config(sections_config, tree_base_path):
     all_an_sorted_sections = []
     for index, sc in enumerate(sections_config):
-        all_an_sorted_sections += alpha_numeric_sort_section(index, sc, tree_base_path)
+        all_an_sorted_sections += alpha_numeric_sort_section(
+            index, sc, tree_base_path
+        )
     return all_an_sorted_sections
 
 
-def return_numerically_sorted_section_for_application(application_display_config, tree_base_path):
+def return_numerically_sorted_section_for_application(
+    application_display_config, tree_base_path
+):
     return {
-        "sorted_sections": sort_sections_from_config(application_display_config, tree_base_path)
+        "sorted_sections": sort_sections_from_config(
+            application_display_config, tree_base_path
+        )
     }
