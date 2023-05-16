fc7466f94f24f2bef5d222059582926c4b119662
RamuniN <navya.ramuni@communities.gov.uk>
2023-05-16 17:51:20 +0000

    added psql client in dockerfile & updated tasks file

--- tasks.py
+++ tasks.py
@@ -2,6 +2,7 @@ from colored import attr
 from colored import fg
 from colored import stylize
 from invoke import task
+import os
 
 ECHO_STYLE = fg("light_gray") + attr("bold")
 
@@ -9,16 +10,24 @@ ECHO_STYLE = fg("light_gray") + attr("bold")
 @task
 def recreate_local_db(c, database_host="localhost", db_name="fsd_fund_store_1"):
     """Create a clean database for testing"""
-    c.run(f"dropdb -h {database_host} --if-exists {db_name} --force")
+    database_url = os.environ.get("DATABASE_URL", f"postgresql://postgres:postgres@{database_host}:5432/{db_name}")
+
+    # As we assume "db_name" is not yet created. First we need to connect to psql with a database
+    # Replace database in database_url with "postgres" db
+    parts = database_url.split('/', 3)
+    parts[3] = "postgres"
+    database_url = '/'.join(parts)
+
+    c.run(f"psql {database_url} -c \"DROP DATABASE IF EXISTS {db_name} WITH (FORCE);\"")
     print(
         stylize(
             f"{db_name} db dropped from {database_host}...",
             ECHO_STYLE,
         )
     )
-    c.run(f"createdb -h {database_host} {db_name}")
+    c.run(f"psql {database_url} -c \"CREATE DATABASE {db_name};\"")
     c.run(
-        f'psql -h {database_host} -d {db_name} -c "create extension if not'
+        f'psql {database_url} -c "create extension if not'
         ' exists ltree;"'
     )
     print(stylize(f"{db_name} db created on {database_host}...", ECHO_STYLE))
@@ -46,6 +55,7 @@ def init_migr(c, database_host="localhost", db_name="fsd_fund_store_1"):
 
 @task
 def seed_db(c, database_host="localhost", db_name="fsd_fund_store_1"):
+    database_url = os.environ.get("DATABASE_URL", f"postgresql://postgres:postgres@{database_host}:5432/{db_name}")
     c.run("flask db upgrade")
     print(
         stylize(
@@ -53,32 +63,32 @@ def seed_db(c, database_host="localhost", db_name="fsd_fund_store_1"):
             ECHO_STYLE,
         )
     )
-    c.run(f"psql -h {database_host} -d {db_name} -a -f db/cof_sql/fund.sql")
-    c.run(f"psql -h {database_host} -d {db_name} -a -f db/cof_sql/rounds.sql")
-    c.run(f"psql -h {database_host} -d {db_name} -a -f db/cof_sql/sections.sql")
+    c.run(f"psql {database_url} -a -f db/cof_sql/fund.sql")
+    c.run(f"psql {database_url} -a -f db/cof_sql/rounds.sql")
+    c.run(f"psql {database_url} -a -f db/cof_sql/sections.sql")
     c.run(
-        f"psql -h {database_host} -d {db_name} -a -f db/cof_sql/assessment_fields.sql"
+        f"psql {database_url} -a -f db/cof_sql/assessment_fields.sql"
     )
-    c.run(f"psql -h {database_host} -d {db_name} -a -f db/cof_sql/section_fields.sql")
+    c.run(f"psql {database_url} -a -f db/cof_sql/section_fields.sql")
     # c.run(
-    #     f"psql -h {database_host} -d {db_name} -a -f"
+    #     f"psql {database_url} -a -f"
     #     " db/cof_sql/translations.sql"
     # )
-    c.run(f"psql -h {database_host} -d {db_name} -a -f db/cof_sql/form_name.sql")
+    c.run(f"psql {database_url} -a -f db/cof_sql/form_name.sql")
     c.run(
-        f'psql -h {database_host} -d {db_name} -c "select f.short_name as'
+        f'psql {database_url} -c "select f.short_name as'
         " Fund, r.short_name as Round from round r join fund f on r.fund_id ="
         ' f.id";'
     )
     c.run(
-        f'psql -h {database_host} -d {db_name} -c "select f.short_name,'
+        f'psql {database_url} -c "select f.short_name,'
         " r.short_name, s.title, s.weighting, s.path from section s join"
         " round r on round_id=r.id join fund f on r.fund_id = f.id order by"
         ' path;"'
     )
 
     c.run(
-        f'psql -h {database_host} -d {db_name} -c "select s.title, f.title'
+        f'psql {database_url} -c "select s.title, f.title'
         " from section s left outer join section_field sf on s.id ="
         " sf.section_id left outer join assessment_field f on sf.field_id ="
         ' f.id order by s.path, sf.display_order;"'
