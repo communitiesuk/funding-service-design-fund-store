8e62b6879b9ee6b347c7f8a239a1ce29972093bf
tferns <thomas.ferns@version1.com>
2023-06-01 14:51:47 +0100

    FS-2701: Fix keys in translations

--- db/migrations/versions/b5f7bf2831de_.py
+++ db/migrations/versions/b5f7bf2831de_.py
@@ -21,7 +21,7 @@ branch_labels = None
 depends_on = None
 
 english_form_name_to_welsh_form_name = {
-    "applicant-information": "gwybodaeth-am-y-ymgeisydd",
+    "applicant-information": "gwybodaeth-am-yr-ymgeisydd",
     "asset-information": "gwybodaeth-am-yr-ased",
     "community-benefits": "buddion-cymunedol",
     "community-engagement": "ymgysylltu-a'r-gymuned",
@@ -29,20 +29,37 @@ english_form_name_to_welsh_form_name = {
     "community-use": "defnydd-cymunedol",
     "declarations": "datganiadau",
     "environmental-sustainability": "cynaliadwyedd-amgylcheddol",
-    "feasibility": "cynhwysiant-ac-integreiddio",
+    "feasibility": "dichonoldeb",
     "funding-required": "cyllid-sydd-ei-angen",
     "inclusiveness-and-integration": "cynhwysiant-ac-integreiddio",
     "local-support": "cefnogaeth-leol",
-    "operational-costs": "costau-gweithredol",
     "organisation-information": "gwybodaeth-am-y-sefydliad",
     "project-costs": "costau'r-prosiect",
     "project-information": "gwybodaeth-am-y-prosiect",
     "project-qualification": "cymhwystra'r-prosiect",
-    "project-qualifications": "cymhwysedd-y-prosiect",
     "risk": "risg",
     "skills-and-resources": "sgiliau-ac-adnoddau",
     "upload-business-plan": "lanlwythwch-y-cynllun-busnes",
     "value-to-the-community": "gwerth-i'r-gymuned",
+    "applicant-information-cof-r3-w1": "gwybodaeth-am-yr-ymgeisydd-cof-r3-w1",
+    "asset-information-cof-r3-w1": "gwybodaeth-am-yr-ased-cof-r3-w1",
+    "community-benefits-cof-r3-w1": "buddion-cymunedol-cof-r3-w1",
+    "community-engagement-cof-r3-w1": "ymgysylltiad-cymunedol-cof-r3-w1",
+    "community-representation-cof-r3-w1": "cynrychiolaeth-gymunedol-cof-r3-w1",
+    "community-use-cof-r3-w1": "defnydd-cymunedol-cof-r3-w1",
+    "declarations-cof-r3-w1": "cadarnhadau-terfynol-cof-r3-w1",
+    "environmental-sustainability-cof-r3-w1": "cynaliadwyedd-amgylcheddol-cof-r3-w1",
+    "feasibility-cof-r3-w1": "dichonoldeb-cof-r3-w1",
+    "funding-required-cof-r3-w1": "cyllid-sydd-ei-angen-cof-r3-w1",
+    "inclusiveness-and-integration-cof-r3-w1": "cynhwysiant-ac-integreiddio-cof-r3-w1",
+    "local-support-cof-r3-w1": "cefnogaeth-leol-cof-r3-w1",
+    "operational-costs-cof-r3-w1": "costau-gweithredol-cof-r3-w1",
+    "organisation-information-cof-r3-w1": "gwybodaeth-am-y-sefydliad-cof-r3-w1",
+    "project-information-cof-r3-w1": "gwybodaeth-am-y-prosiect-cof-r3-w1",
+    "project-qualifications-cof-r3-w1": "cymhwysedd-y-prosiect-cof-r3-w1",
+    "risk-cof-r3-w1": "risg-cof-r3-w1",
+    "skills-and-resources-cof-r3-w1": "sgiliau-ac-adnoddau-cof-r3-w1",
+    "upload-business-plan-cof-r3-w1": "lanlwythwch-y-cynllun-busnes-cof-r3-w1",
 }
 
 
@@ -53,12 +70,7 @@ def upgrade():
 
     for form_name_json, *_ in form_name_entries:
         original_english_form_name = form_name_json["en"]
-        if original_english_form_name.endswith("-cof-r3-w1"):
-            r3 = True
-            english_form_name = original_english_form_name[: -len("-cof-r3-w1")]
-        else:
-            r3 = False
-            english_form_name = original_english_form_name
+        english_form_name = original_english_form_name
 
         is_night_shelter = english_form_name.endswith("-ns")
         if is_night_shelter:
@@ -73,8 +85,6 @@ def upgrade():
         welsh_form_name = welsh_form_name.replace("'", "''")
 
         form_name_json["cy"] = welsh_form_name
-        if r3:
-            form_name_json["cy"] += "-cof-r3-w1"
 
         new_form_name_json_str = json.dumps(form_name_json)
         update_query = sqlalchemy.text(
