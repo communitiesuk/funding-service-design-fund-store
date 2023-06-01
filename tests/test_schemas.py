8c7845fe4470280941a8ec648f53a81b239dc276
tferns <thomas.ferns@version1.com>
2023-06-01 16:35:31 +0100

    FS-2701: Fix & add unit tests

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -1,56 +1,151 @@
-from db.models.section import AssessmentField
+import pytest
+from db.models import FormName
 from db.models.section import Section
-from db.models.section import SectionField
-from db.schemas.section import SectionSchema
+from db.schemas.section import EnglishSectionSchema
+from db.schemas.section import SECTION_SCHEMA_MAP
+from db.schemas.section import WelshSectionSchema
 
-top = Section(
-    id=0,
-    title="Top",
-    path="0",
+section = Section(
+    id=1,
+    title_json={"en": "English Title", "cy": "Welsh Title"},
+    form_name=[
+        FormName(form_name_json={"en": "English Form Name", "cy": "Welsh Form Name"})
+    ],
     children=[
         Section(
             id=1,
-            title="Middle",
-            path="0.1",
-            children=[Section(id=2, title="Bottom", path="0.1.1", children=[])],
+            title_json={"en": "English Child Section 1", "cy": "Welsh Child Section"},
+            path="1.1",
         ),
         Section(
             id=3,
-            title="Middle2",
-            path="0.2",
-            children=[Section(id=4, title="Bottom2", path="0.2.1", children=[])],
+            title_json={"en": "English Child Section 3", "cy": "Welsh Child Section"},
+            path="1.3",
+        ),
+        Section(
+            id=2,
+            title_json={"en": "English Child Section 2", "cy": "Welsh Child Section"},
+            path="1.2",
         ),
     ],
 )
 
+expected_en = {
+    "children": [
+        {
+            "children": [],
+            "fields": [],
+            "form_name": None,
+            "id": 1,
+            "path": "1.1",
+            "title": "English Child Section 1",
+            "title_json": {
+                "cy": "Welsh Child Section",
+                "en": "English Child Section 1",
+            },
+            "weighting": None,
+        },
+        {
+            "children": [],
+            "fields": [],
+            "form_name": None,
+            "id": 2,
+            "path": "1.2",
+            "title": "English Child Section 2",
+            "title_json": {
+                "cy": "Welsh Child Section",
+                "en": "English Child Section 2",
+            },
+            "weighting": None,
+        },
+        {
+            "children": [],
+            "fields": [],
+            "form_name": None,
+            "id": 3,
+            "path": "1.3",
+            "title": "English Child Section 3",
+            "title_json": {
+                "cy": "Welsh Child Section",
+                "en": "English Child Section 3",
+            },
+            "weighting": None,
+        },
+    ],
+    "fields": [],
+    "form_name": "English Form Name",
+    "id": 1,
+    "path": None,
+    "title": "English Title",
+    "title_json": {"cy": "Welsh Title", "en": "English Title"},
+    "weighting": None,
+}
 
-def test_section_serialiser():
-    serialiser = SectionSchema()
-    result = serialiser.dump(top)
-    assert len(result["children"]) == 2
-    assert result["title"] == "Top"
-    assert result["children"][0]["title"] == "Middle"
+expected_cy = {
+    "children": [
+        {
+            "children": [],
+            "fields": [],
+            "form_name": None,
+            "id": 1,
+            "path": "1.1",
+            "title": "Welsh Child Section",
+            "title_json": {
+                "cy": "Welsh Child Section",
+                "en": "English Child Section 1",
+            },
+            "weighting": None,
+        },
+        {
+            "children": [],
+            "fields": [],
+            "form_name": None,
+            "id": 2,
+            "path": "1.2",
+            "title": "Welsh Child Section",
+            "title_json": {
+                "cy": "Welsh Child Section",
+                "en": "English Child Section 2",
+            },
+            "weighting": None,
+        },
+        {
+            "children": [],
+            "fields": [],
+            "form_name": None,
+            "id": 3,
+            "path": "1.3",
+            "title": "Welsh Child Section",
+            "title_json": {
+                "cy": "Welsh Child Section",
+                "en": "English Child Section 3",
+            },
+            "weighting": None,
+        },
+    ],
+    "fields": [],
+    "form_name": "Welsh Form Name",
+    "id": 1,
+    "path": None,
+    "title": "Welsh Title",
+    "title_json": {"cy": "Welsh Title", "en": "English Title"},
+    "weighting": None,
+}
 
 
-with_fields = Section(
-    id=3,
-    title="Middle2",
-    path="0.2",
-    children=[Section(id=4, title="Bottom2", path="0.2.1", children=[])],
-    fields=[
-        SectionField(
-            field_id="abc123",
-            section_id="3",
-            field=AssessmentField(display_type="text", title="my field"),
-        )
+@pytest.mark.parametrize(
+    "section, lang_code, expected",
+    [
+        (section, "en", expected_en),
+        (section, "cy", expected_cy),
     ],
 )
+def test_dump(section, lang_code, expected):
+    schema = SECTION_SCHEMA_MAP[lang_code]()
+    result = schema.dump(section)
+    assert result == expected
 
 
-def test_with_fields():
-    serialiser = SectionSchema()
-    result = serialiser.dump(with_fields)
-    assert len(result["children"]) == 1
-    assert result["title"] == "Middle2"
-    assert len(result["fields"]) == 1
-    assert result["fields"][0]["title"] == "my field"
+def test_section_schema_map():
+    assert SECTION_SCHEMA_MAP["en"] == EnglishSectionSchema
+    assert SECTION_SCHEMA_MAP["cy"] == WelshSectionSchema
