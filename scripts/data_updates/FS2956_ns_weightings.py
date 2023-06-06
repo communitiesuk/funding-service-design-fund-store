fda6e894e402f00b5b7d60725e15e04ccd444d4e
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-06-06 09:57:37 +0100

    fs-2956 adding script to update weightings

---
+++ scripts/data_updates/FS2956_ns_weightings.py
@@ -0,0 +1,41 @@
+import config.fund_loader_config.night_shelter.ns_r2 as ns_r2
+from db import db
+from db.models.section import Section
+from flask import current_app
+from sqlalchemy import update
+
+
+def update_section_weightings(section):
+    current_app.logger.warning(
+        f"\tSection: {section['tree_path']} ({section['section_name']['en']})"
+    )
+    current_app.logger.warning("\t\tUpdating weighting")
+    stmt = (
+        update(Section)
+        .where(Section.path == section["tree_path"])
+        .values(weighting=section["weighting"])
+    )
+
+    db.session.execute(stmt)
+    db.session.commit()
+
+
+def main() -> None:
+    current_app.logger.warning("Updating section weightings for NSTF R2")
+    ns_sections = ns_r2.r2_application_sections
+    sections_to_update = [
+        f"{ns_r2.APPLICATION_BASE_PATH}.3",
+        f"{ns_r2.APPLICATION_BASE_PATH}.4",
+    ]
+    for section in ns_sections:
+        if section["tree_path"] in sections_to_update:
+            update_section_weightings(section)
+    update_section_weightings(ns_r2.rounds_config)
+    current_app.logger.warning("Updates complete")
+
+
+if __name__ == "__main__":
+    from app import app
+
+    with app.app_context():
+        main()
--- tests/test_data_update_scripts.py
+++ tests/test_data_update_scripts.py
@@ -1,3 +1,39 @@
+from db.queries import get_application_sections_for_round
+from db.schemas.section import SectionSchema
+from scripts.data_updates.FS2956_ns_weightings import update_section_weightings
+from db.queries import get_round_by_id
+from db.schemas.round import RoundSchema
+from scripts.data_updates.FS2910_ns_links import update_rounds_with_links
+
+
+def test_update_section_weightings(seed_dynamic_data):
+    sections = get_application_sections_for_round(
+        seed_dynamic_data["funds"][0]["id"],
+        seed_dynamic_data["funds"][0]["rounds"][0]["id"],
+    )
+    section_to_update = None
+    for s in sections:
+        if "skill" in s.title_json["en"]:
+            section_to_update = s
+    assert (
+        section_to_update is not None
+    ), "Unable to find expected test data before updates"
+
+    section_data = SectionSchema().dump(section_to_update)
+    section_data["tree_path"] = section_to_update.path
+    section_data["section_name"] = section_to_update.title_json
+    section_data["weighting"] = "12"
+    update_section_weightings(section_data)
+
+    sections = get_application_sections_for_round(
+        seed_dynamic_data["funds"][0]["id"],
+        seed_dynamic_data["funds"][0]["rounds"][0]["id"],
+    )
+
+    for s in sections:
+        if "skill" in s.title_json["en"]:
+            section = s
+    assert section.weighting == 12
 from db.queries import get_round_by_id
 from db.schemas.round import RoundSchema
 from scripts.data_updates.FS2910_ns_links import update_rounds_with_links
