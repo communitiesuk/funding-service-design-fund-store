4522c4c7a315b3e06946887e6f3bf825cab558b7
Adam Davies <adam.davies@version1.com>
2023-07-19 10:29:52 +0100

    Fix migration

--- db/migrations/versions/024379c68dfe_.py
+++ db/migrations/versions/024379c68dfe_.py
@@ -47,7 +47,7 @@ def upgrade():
 
     with op.batch_alter_table("fund", schema=None) as batch_op:
         batch_op.alter_column(
-            "all_uploaded_documents_section_available", nullable=False
+            "guidance_url", nullable=False
         )
 
     # ### end Alembic commands ###
