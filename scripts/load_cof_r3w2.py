b559d808b00aff11b85d5b3c388259fb7c240f7d
Adam <adam.wallace@communities.gov.uk>
2023-08-10 11:39:23 +0100

    load cof r3w2 script updated

--- scripts/load_cof_r3w2.py
+++ scripts/load_cof_r3w2.py
@@ -1,28 +1,28 @@
 # flake8: noqa
 from config.fund_loader_config.cof.cof_r3 import APPLICATION_BASE_PATH
 from config.fund_loader_config.cof.cof_r3 import ASSESSMENT_BASE_PATH
-from config.fund_loader_config.cof.cof_r3 import cof_r3_sections
-from config.fund_loader_config.cof.cof_r3 import COF_ROUND_3_WINDOW_1_ID
-from config.fund_loader_config.cof.cof_r3 import fund_config
+from config.fund_loader_config.cof.cof_r3 import COF_ROUND_3_WINDOW_2_ID
 from config.fund_loader_config.cof.cof_r3 import round_config
-from db.queries import insert_application_sections
-from db.queries import insert_assessment_sections
 from db.queries import insert_base_sections
-from db.queries import insert_fund_data
 from db.queries import insert_round_data
 
 
 def main() -> None:
-    print("Inserting fund and round data.")
-    insert_fund_data(fund_config)
+    print(
+        "'insert_fund_config(...)' not required as COFR3W2 shares the same fund"
+        " config from COFR3W1."
+    )
+    print("Inserting round data.")
     insert_round_data(round_config)
 
     print("Inserting base sections config.")
     insert_base_sections(
-        APPLICATION_BASE_PATH, ASSESSMENT_BASE_PATH, COF_ROUND_3_WINDOW_1_ID
+        APPLICATION_BASE_PATH, ASSESSMENT_BASE_PATH, COF_ROUND_3_WINDOW_2_ID
     )
-    print("Inserting sections.")
-    insert_application_sections(COF_ROUND_3_WINDOW_1_ID, cof_r3_sections)
+    print(
+        "'insert_application_sections(...)' not required as COFR3W2 shares the same"
+        " sections config from COFR3W1."
+    )
 
 
 if __name__ == "__main__":
