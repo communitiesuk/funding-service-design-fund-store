e47d6a7499f85e386bfe307f3831c3955951b59a
Adam <adam.wallace@communities.gov.uk>
2023-09-15 16:25:22 +0100

    remove redundant tests using old config load

--- tests/test_data_sections.py
+++ tests/test_data_sections.py
@@ -1,28 +1,19 @@
 from typing import List
 
 import pytest
-from config.fund_loader_config.cof.deprecated_fund_config.assessment_section_config import (
-    scored_sections,
-)
-from config.fund_loader_config.cof.deprecated_fund_config.assessment_section_config import (
-    unscored_sections,
-)
-from config.fund_loader_config.cof.deprecated_fund_config.cof_form_config import (
-    COF_R2_ORDERED_FORMS_CONFIG,
-)
-from config.fund_loader_config.cof.deprecated_fund_config.sort_application_sections import (
-    return_numerically_sorted_section_for_application,
-)
-from config.fund_loader_config.cof.deprecated_fund_config.sort_assessment_sections import (
-    return_numerically_sorted_section_for_assessment,
-)
+from config.fund_loader_config.cof.cof_r2 import APPLICATION_BASE_PATH
+from config.fund_loader_config.cof.cof_r2 import ASSESSMENT_BASE_PATH
+from config.fund_loader_config.cof.cof_r2 import cof_r2_sections
+from config.fund_loader_config.cof.cof_r2 import COF_ROUND_2_WINDOW_2_ID
+from config.fund_loader_config.cof.cof_r2 import fund_config
+from config.fund_loader_config.cof.cof_r2 import rounds_config
 from db.models.section import Section
 from db.queries import get_application_sections_for_round
 from db.queries import get_assessment_sections_for_round
-from db.queries import insert_assessment_sections
+from db.queries import insert_base_sections
+from db.queries import insert_fund_data
 from db.queries import insert_or_update_application_sections
-from db.queries import upsert_fields
-from fsd_test_utils.test_config.useful_config import UsefulConfig
+from db.queries import insert_round_data
 
 
 def test_get_application_sections(seed_dynamic_data):
@@ -53,26 +44,20 @@ def test_get_assessment_sections(seed_dynamic_data):
     assert len(sections[0].children[0].children) == 0
 
 
-def test_load_application_sections(seed_dynamic_data):
-    sorted_application_sections = return_numerically_sorted_section_for_application(
-        COF_R2_ORDERED_FORMS_CONFIG, "0.1"
-    )["sorted_sections"]
-    result = insert_or_update_application_sections(
-        seed_dynamic_data["funds"][0]["rounds"][0]["id"], sorted_application_sections
+def test_load_application_sections(clear_test_data):
+    insert_fund_data(fund_config)
+    insert_round_data(rounds_config)
+
+    base_sections = insert_base_sections(
+        APPLICATION_BASE_PATH, ASSESSMENT_BASE_PATH, COF_ROUND_2_WINDOW_2_ID
     )
-    assert len(result) == 28
+    application_sections = insert_or_update_application_sections(
+        COF_ROUND_2_WINDOW_2_ID, cof_r2_sections
+    )
+    assert len(base_sections) == 2
+    assert len(application_sections) == 28
 
 
 @pytest.mark.skip(reason="not implemented assessment loading yet")
 def test_load_assessment_sections():
-    # input config
-    assessment_config = return_numerically_sorted_section_for_assessment(
-        scored_sections, unscored_sections
-    )
-
-    inserted_field_ids = upsert_fields(assessment_config["all_fields"])
-    result = insert_assessment_sections(UsefulConfig.COF_ROUND_2_ID, assessment_config)
-
-    assert len(inserted_field_ids) == 124
-    assert len(result["inserted_sections"]) == 53
-    assert len(result["inserted_section_field_links"]) == 124
+    pass
