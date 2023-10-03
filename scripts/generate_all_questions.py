d1a1d49f8c78aa21bd438c267d959599ca63e3d0
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-10-03 11:21:14 +0100

    fs-3428 headings working

--- scripts/generate_all_questions.py
+++ scripts/generate_all_questions.py
@@ -11,6 +11,11 @@ from db.queries import (  # noqa: E402
 )  # noqa: E402
 from app import create_app  # noqa: E402
 from airium import Airium  # noqa: E402
+import os.path  # noqa: E402
+import json  # noqa: E402
+from bs4 import BeautifulSoup  # noqa: E402
+from scripts.read_forms import strip_leading_numbers, build_form  # noqa: E402
+
 
 air = Airium()
 
@@ -35,16 +40,136 @@ BOILERPLATE_END = """
 """
 
 
-def print_html(section_headers: dict) -> str:
+def create_toc(air: Airium, section_headers: dict):
+    with air.h2(klass="govuk-heading-m "):
+        air("{% trans %}Table of contents{% endtrans %}")
+    with air.ol(klass="govuk-list govuk-list--number"):
+        for anchor, text in section_headers.items():
+            with air.li():
+                with air.a(klass="govuk-link", href=f"#{anchor}"):
+                    air(text)
+
+
+def find_forms_dir(path_to_form_jsons, fund_short_name, round_short_name, lang):
+    round_folder_path = os.path.join(
+        path_to_form_jsons,
+        f"{fund_short_name.casefold()}_{round_short_name.casefold()}",
+    )
+    if not os.path.isdir(round_folder_path):
+        print(f"ERROR Could not find form_jsons at {round_folder_path}")
+
+    path_with_lang = os.path.join(round_folder_path, lang)
+    if not os.path.isdir(path_with_lang):
+        return round_folder_path
+    else:
+        return path_with_lang
+
+
+def build_section_header(section: Section, lang="en"):
+    title = section.title_json[lang]
+    title = strip_leading_numbers(title)
+    anchor = title.casefold().replace(" ", "-")
+    return anchor, title
+    # section_header[anchor] = title
+    # section_headers[f"{anchor}_form_name"]= s["form_name"]["form_name_json"]["lang"]
+    # return section_header
+
+
+def build_components_from_page(p, include_html_components=True):
+    components = []
+    for c in p["components"]:
+        text = []
+        if (
+            include_html_components
+            and ("type" in c)
+            and (c["type"].casefold() == "html" or c["type"].casefold() == "para")
+        ):
+            text = [c["content"]]
+        elif "hint" in c:
+            soup = BeautifulSoup(c["hint"], "html.parser")
+            text = [e.text for e in soup.children]
+        component = {
+            "title": c["title"] if "title" in c else None,
+            "text": text,
+            "hide_title": c["options"]["hideTitle"]
+            if "hideTitle" in c["options"]
+            else False,
+        }
+        components.append(component)
+    return components
+
+
+def build_questions_for_form(air: Airium, form_name: str, path_to_forms: str):
+    path_to_form = os.path.join(path_to_forms, f"{form_name}.json")
+    with open(path_to_form, "r") as f:
+        form_data = json.load(f)
+
+    pages = {}
+    index = 1
+    start_page = form_data["startPage"]
+    first_page = next(p for p in form_data["pages"] if p["path"] == start_page)
+    is_just_html_start_page = all(
+        [
+            (c["type"].casefold() == "para" or c["type"].casefold() == "html")
+            for c in first_page["components"]
+        ]
+    )
+    if is_just_html_start_page:
+        real_first_page_path = first_page["next"][0]["path"]
+        real_first_page = next(
+            p for p in form_data["pages"] if p["path"] == real_first_page_path
+        )
+        form_first_page = {
+            "title": real_first_page["title"],
+            "components": build_components_from_page(
+                real_first_page, include_html_components=False
+            ),
+        }
+    else:
+        form_first_page = {
+            "title": first_page["title"],
+            "components": build_components_from_page(
+                first_page, include_html_components=True
+            ),
+        }
+    for p in form_data["pages"]:
+        if p["path"] == "/summary" or p["path"] == start_page:
+            continue
+        components = build_components_from_page(p)
+
+        page = {"title": p["title"], "components": components}
+        pages[index] = page
+        index += 1
+    return form_first_page, pages
+
+
+def print_components(air: Airium, components: list, level_above):
+    idx_component = 1
+    for c in components:
+        if not c["hide_title"]:
+            with air.h4(klass="govuk-heading-s"):
+                air(f"{level_above}{idx_component}. {c['title']}")
+
+        for t in c["text"]:
+            with air.p(klass="govuk-body"):
+                air(t)
+
+
+def print_html(
+    sections: list[Section], fund_short_name, round_short_name, lang, path_to_form_jsons
+) -> str:
+    section_headers = {}
+    section_children = {}
+    for s in sections:
+        anchor, text = build_section_header(s)
+        section_headers[anchor] = text
+        section_children[anchor] = s.children
     with air.div(klass="govuk-!-margin-bottom-8"):
-        with air.h2(klass="govuk-heading-m "):
-            air("{% trans %}Table of contents{% endtrans %}")
-        with air.ol(klass="govuk-list govuk-list--number"):
-            for anchor, text in section_headers.items():
-                with air.li():
-                    with air.a(klass="govuk-link", href=f"#{anchor}"):
-                        air(text)
-        index = 1
+        create_toc(air, section_headers)
+        forms_dir = find_forms_dir(
+            path_to_form_jsons, fund_short_name, round_short_name, lang
+        )
+        idx_section = 1
         for anchor, text in section_headers.items():
             air.hr(
                 klass=(
@@ -53,26 +178,43 @@ def print_html(section_headers: dict) -> str:
                 )
             )
             with air.h2(klass="govuk-heading-l", id=anchor):
-                air(f"{index}. {text}")
-            index += 1
+                air(f"{idx_section}. {text}")
+
+            idx_form = 1
+            for child_form in section_children[anchor]:
+                form_name = child_form.form_name[0].form_name_json[lang]
+
+                form_index = build_form(form_name=form_name, path_to_forms=forms_dir)
+
+                for page_idx, page in form_index.items():
+                    if page_idx == 1:
+                        with air.h3(klass="govuk-heading-m"):
+                            air(f"{idx_section}.{idx_form}. {page['page_title']}")
+                    else:
+                        with air.h4(klass="govuk-heading-s"):
+                            air(
+                                f"{idx_section}.{idx_form}.{page_idx-1}."
+                                f" {page['page_title']}"
+                            )
+
+                    # print page["page_children"]
+
+                # form_first_page, pages = build_questions_for_form(air, form_name, forms_dir)
+                # with air.h3(klass="govuk-heading-m"):
+                #     air(f"{idx_section}.{idx_form}. {form_first_page['title']}")
+                # print_components(air, form_first_page["components"], f"{idx_section}.{idx_form}.")
+                # for idx_page, page in pages.items():
+                #     with air.h4(klass="govuk-heading-s"):
+                #         air(f"{idx_section}.{idx_form}.{idx_page}. {page['title']}")
+                #     print_components(air,page["components"], f"{idx_section}.{idx_form}.{idx_page}.")
+                idx_form += 1
+
+            idx_section += 1
 
     html = f"{BOILERPLATE_START}{str(air)}{BOILERPLATE_END}"
     print(html)
+    print(forms_dir)
     return html
-
-
-def build_section_headers(sections: list[Section], lang):
-    section_headers = {}
-    for s in sections:
-        title = s.title_json[lang]
-        i = title.find(". ")
-        if i != -1:
-            title = title[i + 2 :]
-        else:
-            title = title
-        anchor = title.casefold().replace(" ", "-")
-        section_headers[anchor] = title
-    return section_headers
 
 
 @click.command()
@@ -87,21 +229,37 @@ def build_section_headers(sections: list[Section], lang):
 )
 @click.option(
     "--output_location",
-    default="/Users/sarahsloan/dev/temp/",
+    default="/Users/sarahsloan/dev/CommunitiesUkWorkspace/funding-service-design-frontend/app/templates/",
     help="Folder to write output html to",
     prompt=True,
 )
+@click.option(
+    "--forms_dir",
+    default="/Users/sarahsloan/dev/CommunitiesUkWorkspace/digital-form-builder/fsd_config/form_jsons/",
+    help="Local, absolute path, to the form JSONs to use to generate question lists",
+    prompt=True,
+)
 def generate_all_questions(
-    fund_short_code=None, round_short_code=None, lang=None, output_location=None
+    fund_short_code=None,
+    round_short_code=None,
+    lang=None,
+    output_location=None,
+    forms_dir=None,
 ):
     app = create_app()
     with app.app_context():
         round = get_round_by_short_name(fund_short_code, round_short_code)
         sections = get_application_sections_for_round(round.fund_id, round.id)
 
-        section_headers = build_section_headers(sections, lang)
+        # section_headers = build_section_headers(sections, lang)
 
-        html_str = print_html(section_headers=section_headers)
+        html_str = print_html(
+            sections,
+            fund_short_name=fund_short_code,
+            round_short_name=round_short_code,
+            lang=lang,
+            path_to_form_jsons=forms_dir,
+        )
         filename = f"{fund_short_code.casefold()}_{round_short_code.casefold()}_all_questions.html"
         with open(f"{output_location}{filename}", "w") as f:
             f.write(html_str)
---
+++ tests/test_generate_all_questions.py
@@ -0,0 +1,117 @@
+import json
+import os
+
+import pytest
+from db.models.section import Section
+from scripts.generate_all_questions import build_section_header
+from scripts.generate_all_questions import find_forms_dir
+from scripts.read_forms import build_display_for_form
+from scripts.read_forms import build_page_index
+from scripts.read_forms import strip_leading_numbers
+
+
+@pytest.mark.parametrize(
+    "text,exp",
+    [
+        ("5.1 hello", "hello"),
+        ("5.1. hello", "hello"),
+        ("55.1. hello", "hello"),
+        ("5.13. hello", "hello"),
+        ("hello", "hello"),
+    ],
+)
+def test_strip_leading_numbers(text, exp):
+    assert strip_leading_numbers(text) == exp
+
+
+def test_build_display_for_form_cyp_r1_risk():
+
+    path_to_form = (
+        "/Users/sarahsloan/dev/CommunitiesUkWorkspace/digital-form-builder/"
+        "fsd_config/form_jsons/cyp_r1/risk-and-deliverability-cyp.json"
+    )
+    with open(path_to_form, "r") as f:
+        form_data = json.load(f)
+
+    result = build_display_for_form(form_data)
+    assert len(result) == 4
+
+
+def test_build_page_index_cyp_r1_risk():
+
+    path_to_form = (
+        "/Users/sarahsloan/dev/CommunitiesUkWorkspace/digital-form-builder/"
+        "fsd_config/form_jsons/cyp_r1/risk-and-deliverability-cyp.json"
+    )
+    with open(path_to_form, "r") as f:
+        form_data = json.load(f)
+
+    index = build_page_index(form_data)
+    assert len(index) == 4
+    assert index[4]["path"] == "/organisation-governance-structure"
+
+
+# def test_build_questions_cyp_r1_name_your_application():
+#     path = "/Users/sarahsloan/dev/CommunitiesUkWorkspace/digital-form-builder/fsd_config/form_jsons/cyp_r1/"
+#     pages = build_questions(None, "name-your-application-cyp", path)
+#     assert len(pages) == 1
+#     assert pages[1]["title"] == "Name your application"
+#     assert len(pages[1]["components"]) == 1
+#     assert pages[1]["components"][0]["title"] == "Name your application"
+#     assert pages[1]["components"][0]["hide_title"] is True
+#     assert len(pages[1]["components"][0]["text"]) == 2
+
+
+@pytest.mark.parametrize(
+    "section,lang,exp_anchor,exp_text",
+    [
+        (
+            Section(title_json={"en": "Before You Start"}),
+            "en",
+            "before-you-start",
+            "Before You Start",
+        ),
+        (
+            Section(title_json={"en": "1. Before You Start"}),
+            "en",
+            "before-you-start",
+            "Before You Start",
+        ),
+        (
+            Section(title_json={"en": "Before You Start", "cy": "Welsh"}),
+            "cy",
+            "welsh",
+            "Welsh",
+        ),
+        (
+            Section(title_json={"en": "Before You Start", "cy": "10. Welsh"}),
+            "cy",
+            "welsh",
+            "Welsh",
+        ),
+    ],
+)
+def test_build_section_headers(section, lang, exp_anchor, exp_text):
+    res_anchor, res_text = build_section_header(section, lang)
+    assert res_anchor == exp_anchor
+    assert res_text == exp_text
+
+
+def test_find_forms_dir_no_lang(tmp_path):
+    temp_json_dir = os.path.join(tmp_path, "form_jsons")
+    os.mkdir(temp_json_dir)
+    round_dir = os.path.join(temp_json_dir, "f1_r1")
+    os.mkdir(round_dir)
+    result = find_forms_dir(temp_json_dir, "f1", "r1", "en")
+    assert result == round_dir
+
+
+def test_find_forms_dir_with_lang(tmp_path):
+    temp_json_dir = os.path.join(tmp_path, "form_jsons")
+    os.mkdir(temp_json_dir)
+    round_dir = os.path.join(temp_json_dir, "f1_r1")
+    os.mkdir(round_dir)
+    round_dir = os.path.join(round_dir, "en")
+    os.mkdir(round_dir)
+    result = find_forms_dir(temp_json_dir, "f1", "r1", "en")
+    assert result == round_dir
