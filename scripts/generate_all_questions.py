6dbab2f083c885e6e701b260937d30731d7cf6af
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-10-09 12:27:56 +0100

    Generating correct form headers

--- scripts/generate_all_questions.py
+++ scripts/generate_all_questions.py
@@ -14,7 +14,12 @@ from airium import Airium  # noqa: E402
 import os.path  # noqa: E402
 import json  # noqa: E402
 from bs4 import BeautifulSoup  # noqa: E402
-from scripts.read_forms import strip_leading_numbers  # , build_form  # noqa: E402
+from scripts.read_forms import (  # noqa: E402
+    increment_lowest_in_hierarchy,  # noqa: E402
+    strip_leading_numbers,  # noqa: E402
+)  # , build_form  # noqa: E402
+from scripts.all_questions.metadata_utils import generate_index  # noqa: E402
+from scripts.all_questions.metadata_utils import generate_metadata  # noqa: E402
 
 
 air = Airium()
@@ -40,14 +45,14 @@ BOILERPLATE_END = """
 """
 
 
-def create_toc(air: Airium, section_headers: dict):
+def print_html_toc(air: Airium, sections: dict):
     with air.h2(klass="govuk-heading-m "):
         air("{% trans %}Table of contents{% endtrans %}")
     with air.ol(klass="govuk-list govuk-list--number"):
-        for anchor, text in section_headers.items():
+        for anchor, details in sections.items():
             with air.li():
                 with air.a(klass="govuk-link", href=f"#{anchor}"):
-                    air(text)
+                    air(details["title_text"])
 
 
 def find_forms_dir(path_to_form_jsons, fund_short_name, round_short_name, lang):
@@ -155,22 +160,35 @@ def print_components(air: Airium, components: list, level_above):
                 air(t)
 
 
-def print_html(
-    sections: list[Section], fund_short_name, round_short_name, lang, path_to_form_jsons
-) -> str:
-    section_headers = {}
-    section_children = {}
-    for s in sections:
-        anchor, text = build_section_header(s)
-        section_headers[anchor] = text
-        section_children[anchor] = s.children
+def print_html_for_page(air: Airium, page, all_pages, this_idx, form_json_page):
+    title = strip_leading_numbers(form_json_page["title"])
+    with air.h3(klass="govuk-heading-m"):
+        air(f"{this_idx}. {title}")
+
+
+def print_html_for_form(air: Airium, section_idx: int, form_metadata):
+    start_page_path = form_metadata["start_page"]
+    # index = form_metadata["index"]
+    # start_page = next(
+    #     p for p in form_metadata["all_pages"] if p["path"] == start_page_path
+    # )
+    form_json_page = next(
+        p for p in form_metadata["full_json"]["pages"] if p["path"] == start_page_path
+    )
+
+    title = strip_leading_numbers(form_json_page["title"])
+    with air.h3(klass="govuk-heading-m"):
+        air(f"{section_idx}. {title}")
+
+    # idx = increment_lowest_in_hierarchy(f"{section_idx}.0")
+    # print_html_for_page(air, start_page, form_metadata, this_idx=idx, form_json_page=form_json_page)
+
+
+def print_html(sections: dict, lang) -> str:
     with air.div(klass="govuk-!-margin-bottom-8"):
-        create_toc(air, section_headers)
-        forms_dir = find_forms_dir(
-            path_to_form_jsons, fund_short_name, round_short_name, lang
-        )
+        print_html_toc(air, sections)
         idx_section = 1
-        for anchor, text in section_headers.items():
+        for anchor, details in sections.items():
             air.hr(
                 klass=(
                     "govuk-section-break govuk-section-break--l"
@@ -178,45 +196,52 @@ def print_html(
                 )
             )
             with air.h2(klass="govuk-heading-l", id=anchor):
-                air(f"{idx_section}. {text}")
+                air(f"{idx_section}. {details['title_text']}")
 
-            idx_form = 1
-            for child_form in section_children[anchor]:
-                # form_name = child_form.form_name[0].form_name_json[lang]
+            form_idx = f"{idx_section}.0"
+            for form in details["forms"]:
+                form_idx = increment_lowest_in_hierarchy(form_idx)
+                print_html_for_form(air, form_idx, form)
 
-                form_index = (
-                    {}
-                )  # build_form(form_name=form_name, path_to_forms=forms_dir)
+            idx_section += 1
 
-                for page_idx, page in form_index.items():
-                    if page_idx == 1:
-                        with air.h3(klass="govuk-heading-m"):
-                            air(f"{idx_section}.{idx_form}. {page['page_title']}")
-                    else:
-                        with air.h4(klass="govuk-heading-s"):
-                            air(
-                                f"{idx_section}.{idx_form}.{page_idx}."
-                                f" {page['page_title']}"
-                            )
+            continue
+            # idx_form = 1
+            # for child_form in section_children[anchor]:
+            #     # form_name = child_form.form_name[0].form_name_json[lang]
 
-                    print_components(air, page["page_children"], 0)
-                    # print page["page_children"]
+            #     form_index = (
+            #         {}
+            #     )  # build_form(form_name=form_name, path_to_forms=forms_dir)
 
-                # form_first_page, pages = build_questions_for_form(air, form_name, forms_dir)
-                # with air.h3(klass="govuk-heading-m"):
-                #     air(f"{idx_section}.{idx_form}. {form_first_page['title']}")
-                # print_components(air, form_first_page["components"], f"{idx_section}.{idx_form}.")
-                # for idx_page, page in pages.items():
-                #     with air.h4(klass="govuk-heading-s"):
-                #         air(f"{idx_section}.{idx_form}.{idx_page}. {page['title']}")
-                #     print_components(air,page["components"], f"{idx_section}.{idx_form}.{idx_page}.")
-                idx_form += 1
+            #     for page_idx, page in form_index.items():
+            #         if page_idx == 1:
+            #             with air.h3(klass="govuk-heading-m"):
+            #                 air(f"{idx_section}.{idx_form}. {page['page_title']}")
+            #         else:
+            #             with air.h4(klass="govuk-heading-s"):
+            #                 air(
+            #                     f"{idx_section}.{idx_form}.{page_idx}."
+            #                     f" {page['page_title']}"
+            #                 )
+
+            #         print_components(air, page["page_children"], 0)
+            #         # print page["page_children"]
+
+            #     # form_first_page, pages = build_questions_for_form(air, form_name, forms_dir)
+            #     # with air.h3(klass="govuk-heading-m"):
+            #     #     air(f"{idx_section}.{idx_form}. {form_first_page['title']}")
+            #     # print_components(air, form_first_page["components"], f"{idx_section}.{idx_form}.")
+            #     # for idx_page, page in pages.items():
+            #     #     with air.h4(klass="govuk-heading-s"):
+            #     #         air(f"{idx_section}.{idx_form}.{idx_page}. {page['title']}")
+            #     #     print_components(air,page["components"], f"{idx_section}.{idx_form}.{idx_page}.")
+            #     idx_form += 1
 
             idx_section += 1
 
     html = f"{BOILERPLATE_START}{str(air)}{BOILERPLATE_END}"
     print(html)
-    print(forms_dir)
     return html
 
 
@@ -252,16 +277,51 @@ def generate_all_questions(
     app = create_app()
     with app.app_context():
         round = get_round_by_short_name(fund_short_code, round_short_code)
-        sections = get_application_sections_for_round(round.fund_id, round.id)
+        sections: list[Section] = get_application_sections_for_round(
+            round.fund_id, round.id
+        )
 
         # section_headers = build_section_headers(sections, lang)
+        path_to_form_jsons = find_forms_dir(
+            forms_dir, fund_short_code, round_short_code, lang
+        )
+
+        section_map = {}
+
+        for section in sections:
+            anchor, text = build_section_header(section, lang=lang)
+            form_metadatas = []
+            for child_form in section.children:
+                form_name = child_form.form_name[0].form_name_json[lang]
+                path_to_form = os.path.join(path_to_form_jsons, f"{form_name}.json")
+                with open(path_to_form, "r") as f:
+                    form_data = json.load(f)
+                    form_metadata = generate_metadata(form_data)
+                    form_index = {}
+
+                    first_page = next(
+                        p
+                        for p in form_metadata["all_pages"]
+                        if p["path"] == form_metadata["start_page"]
+                    )
+                    generate_index(
+                        page=first_page,
+                        results=form_index,
+                        idx=1,
+                        all_pages=form_metadata["all_pages"],
+                        start_page=True,
+                    )
+                    form_metadata["index"] = form_index
+                    form_metadata["full_json"] = form_data
+                    form_metadatas.append(form_metadata)
+            section_map[anchor] = {
+                "title_text": text,
+                "forms": form_metadatas,
+            }
 
         html_str = print_html(
-            sections,
-            fund_short_name=fund_short_code,
-            round_short_name=round_short_code,
+            sections=section_map,
             lang=lang,
-            path_to_form_jsons=forms_dir,
         )
         filename = f"{fund_short_code.casefold()}_{round_short_code.casefold()}_all_questions.html"
         with open(f"{output_location}{filename}", "w") as f:
