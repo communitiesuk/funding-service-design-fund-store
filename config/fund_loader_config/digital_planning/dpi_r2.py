d529d6b8fc27cd31226ecb4e972624f07e6b1187
Adam Davies <adam.davies@version1.com>
2023-10-12 16:21:35 +0100

    Updating open and close time

--- config/fund_loader_config/digital_planning/dpi_r2.py
+++ config/fund_loader_config/digital_planning/dpi_r2.py
@@ -11,10 +11,10 @@ DPI_ROUND_2_ID = "0059aad4-5eb5-11ee-8c99-0242ac120002"
 APPLICATION_BASE_PATH = ".".join([str(DPI_R2_BASE_PATH), str(1)])
 ASSESSMENT_BASE_PATH = ".".join([str(DPI_R2_BASE_PATH), str(2)])
 DPI_R2_OPENS_DATE = datetime(
-    2023, 10, 17, 10, 0, 0, tzinfo=timezone.utc
+    2023, 10, 17, 9, 30, 0, tzinfo=timezone.utc
 )  # 2023-10-17 10:00:00
 DPI_R2_DEADLINE_DATE = datetime(
-    2023, 12, 1, 11, 59, 0, tzinfo=timezone.utc
+    2023, 12, 1, 17, 0, 0, tzinfo=timezone.utc
 )  # 2023-12-1 11:59:00
 DPI_R2_ASSESSMENT_DEADLINE_DATE = datetime(
     2024, 1, 31, 12, 0, 0, tzinfo=timezone.utc
