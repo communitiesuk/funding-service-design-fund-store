ad9f6d7052959e43b978aa642c404ac4f19cfda2
RamuniN <navya.ramuni@communities.gov.uk>
2024-02-20 09:42:28 +0000

    update key name

--- config/fund_loader_config/cof/eoi_r1_schema.py
+++ config/fund_loader_config/cof/eoi_r1_schema.py
@@ -105,9 +105,9 @@ COF_R3_EOI_SCHEMA = {
             "caveat": None,
         },
     ],
-    "daJkaD": [
+    "BykoQQ": [
         {
-            "answerValue": ["Not sure"],
+            "answerValue": ["none"],
             "result": Eoi_Decision.PASS_WITH_CAVEATS,
             "caveat": (
                 "Make progress in securing match funding: COF will contribute up to 80%"
