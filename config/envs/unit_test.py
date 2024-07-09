c0ffeba153f829d9231a631b356dc63f8bb9f356
NarenderRajuB <bnraju81@gmail.com>
2024-07-10 00:30:25 +0100

    fix tests

--- config/envs/unit_test.py
+++ config/envs/unit_test.py
@@ -13,7 +13,6 @@ class UnitTestConfig(Config):
     #  Application Config
     SECRET_KEY = "test"  # pragma: allowlist secret
     SESSION_COOKIE_NAME = "session_cookie"
-    SESSION_COOKIE_SAMESITE = None
 
     # Logging
     FSD_LOG_LEVEL = logging.DEBUG
@@ -21,5 +20,5 @@ class UnitTestConfig(Config):
     # Database
     SQLALCHEMY_DATABASE_URI = environ.get(
         "DATABASE_URL",
-        "postgresql://postgres:password@127.0.0.1:5432/fsd_fund_store_unit_test",  # pragma: allowlist secret
+        "postgresql://postgres:postgres@127.0.0.1:5432/fsd_fund_store_unit_test",  # pragma: allowlist secret
     )
