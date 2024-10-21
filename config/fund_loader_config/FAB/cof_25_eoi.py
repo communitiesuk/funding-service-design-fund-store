e3a70824b286b5714800d98e2240a15e13046f72
srh-sloan <srh-sloan@users.noreply.github.com>
2024-10-21 13:46:43 +0000

    removing noqa as it breaks scripts

--- config/fund_loader_config/FAB/cof_25_eoi.py
+++ config/fund_loader_config/FAB/cof_25_eoi.py
@@ -1,4 +1,4 @@
-LOADER_CONFIG = {
+LOADER_CONFIG={
     "sections_config": [
         {
             "section_name": {"en": "1. Expression of interest", "cy": ""},
@@ -42,7 +42,7 @@ LOADER_CONFIG = {
         "name_json": {"en": "Community Ownership Fund 2025"},
         "title_json": {"en": "expression of interest in applying for the Community Ownership Fund 2025"},
         "description_json": {
-            "en": "The Community Ownership Fund is a £150 million fund over 4 years to support community groups across England, Wales, Scotland and Northern Ireland to take ownership of assets which are at risk of being lost to the community."  # noqa:E501
+            "en": "The Community Ownership Fund is a £150 million fund over 4 years to support community groups across England, Wales, Scotland and Northern Ireland to take ownership of assets which are at risk of being lost to the community."
         },
     },
     "round_config": {
@@ -55,8 +55,8 @@ LOADER_CONFIG = {
         "application_reminder_sent": False,
         "reminder_date": "2024-12-01T11:59:00",
         "assessment_deadline": "2025-01-31T17:00:00",
-        "prospectus": "https://www.gov.uk/government/publications/community-ownership-fund-prospectus/community-ownership-fund-2025-prospectus",  # noqa:E501
-        "privacy_notice": "https://www.gov.uk/government/publications/community-ownership-fund-privacy-notice/community-ownership-fund-privacy-notice",  # noqa:E501
+        "prospectus": "https://www.gov.uk/government/publications/community-ownership-fund-prospectus/community-ownership-fund-2025-prospectus",
+        "privacy_notice": "https://www.gov.uk/government/publications/community-ownership-fund-privacy-notice/community-ownership-fund-privacy-notice",
         "reference_contact_page_over_email": False,
         "contact_email": "COF@communities.gov.uk",
         "contact_phone": "0",
@@ -74,11 +74,11 @@ LOADER_CONFIG = {
         "mark_as_complete_enabled": False,
         "is_expression_of_interest": True,
         "eoi_decision_schema": None,
-        "feedback_survey_config": "{'has_feedback_survey': '', 'has_section_feedback': False, 'is_feedback_survey_optional': False, 'is_section_feedback_optional': False}",  # noqa:E501
+        "feedback_survey_config": "{'has_feedback_survey': '', 'has_section_feedback': False, 'is_feedback_survey_optional': False, 'is_section_feedback_optional': False}",
         "eligibility_config": {"has_eligibility": False},
         "title_json": {"en": "Round 1"},
         "contact_us_banner_json": {
-            "en": '{"en":\'<h2 class="govuk-heading-m">Get application support</h2>\r\n                 <p class="govuk-body">\r\n                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund">Visit the My Community website</a>\r\n                    for information and guidance on applying to Community Ownership Fund.\r\n                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund#enquiry-form">Fill out the enquiry form</a>\r\n                    to request advice from My Community.\r\n                </p>\r\n                <p class="govuk-body">\r\n                    We cannot provide direct support to applicants outside of this service.\r\n                </p>\r\n                <h2 class="govuk-heading-m">Get technical support</h2>\r\n                <p  class="govuk-body">\r\n                    Contact the Ministry of Housing, Communities & Local Government funding team if you need\r\n                    help with accessing or submitting an application form.\r\n                </p>\',\r\n"cy": \'<h2 class="govuk-heading-m">Cael cymorth â\'r cais</h2>\r\n                 <p class="govuk-body">\r\n                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund">Ewch i wefan My Community</a>\r\n                    i gael gwybodaeth ac arweiniad ar wneud cais i\'r Gronfa Perchnogaeth Gymunedol.\r\n                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund#enquiry-form">Llenwch y ffurflen ymholiad</a>\r\n                    i ofyn am gyngor gan My Community.\r\n                </p>\r\n                <p class="govuk-body">\r\n                    Ni allwn ddarparu cymorth uniongyrchol i ymgeiswyr tu hwnt i\'r gwasanaeth hwn.\r\n                </p>\r\n                <h2 class="govuk-heading-m">Cael cymorth technegol</h2>\r\n                <p  class="govuk-body">\r\n                    Cysylltwch â thîm cyllid yr Adran Ffyniant Bro, Tai a Chymunedau os oes angen help arnoch i gael at ffurflen gais neu ei chyflwyno.\r\n                </p>\'}',  # noqa:E501
+            "en": '{"en":\'<h2 class="govuk-heading-m">Get application support</h2>\r\n                 <p class="govuk-body">\r\n                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund">Visit the My Community website</a>\r\n                    for information and guidance on applying to Community Ownership Fund.\r\n                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund#enquiry-form">Fill out the enquiry form</a>\r\n                    to request advice from My Community.\r\n                </p>\r\n                <p class="govuk-body">\r\n                    We cannot provide direct support to applicants outside of this service.\r\n                </p>\r\n                <h2 class="govuk-heading-m">Get technical support</h2>\r\n                <p  class="govuk-body">\r\n                    Contact the Ministry of Housing, Communities & Local Government funding team if you need\r\n                    help with accessing or submitting an application form.\r\n                </p>\',\r\n"cy": \'<h2 class="govuk-heading-m">Cael cymorth â\'r cais</h2>\r\n                 <p class="govuk-body">\r\n                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund">Ewch i wefan My Community</a>\r\n                    i gael gwybodaeth ac arweiniad ar wneud cais i\'r Gronfa Perchnogaeth Gymunedol.\r\n                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund#enquiry-form">Llenwch y ffurflen ymholiad</a>\r\n                    i ofyn am gyngor gan My Community.\r\n                </p>\r\n                <p class="govuk-body">\r\n                    Ni allwn ddarparu cymorth uniongyrchol i ymgeiswyr tu hwnt i\'r gwasanaeth hwn.\r\n                </p>\r\n                <h2 class="govuk-heading-m">Cael cymorth technegol</h2>\r\n                <p  class="govuk-body">\r\n                    Cysylltwch â thîm cyllid yr Adran Ffyniant Bro, Tai a Chymunedau os oes angen help arnoch i gael at ffurflen gais neu ei chyflwyno.\r\n                </p>\'}',
             "cy": "",
         },
     },
