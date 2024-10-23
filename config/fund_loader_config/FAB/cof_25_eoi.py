fc1755aa0d255a821472172e285155abf4c96ad9
srh-sloan <sarah.sloan@communities.gov.uk>
2024-10-23 09:16:03 +0000

    fs-4684 adding welsh config

--- config/fund_loader_config/FAB/cof_25_eoi.py
+++ config/fund_loader_config/FAB/cof_25_eoi.py
@@ -1,48 +1,87 @@
 LOADER_CONFIG={
     "sections_config": [
         {
-            "section_name": {"en": "1. Expression of interest", "cy": ""},
+            "section_name": {
+                "en": "1. Expression of interest",
+                "cy": "1. Mynegi diddordeb",
+            },
             "tree_path": "1037.1.1",
-            "requires_feedback": None,
+            "requires_feedback": True,
         },
         {
-            "section_name": {"en": "1.1 Organisation details", "cy": ""},
+            "section_name": {
+                "en": "1.1 Organisation details",
+                "cy": "1.1 Manylion y sefydliad",
+            },
             "tree_path": "1037.1.1.1",
-            "form_name_json": {"en": "organisation-details-25", "cy": ""},
+            "form_name_json": {
+                "en": "organisation-details-25",
+                "cy": "manylion-y-sefydliad",
+            },
         },
         {
-            "section_name": {"en": "1.2 About your asset", "cy": ""},
+            "section_name": {
+                "en": "1.2 About your asset",
+                "cy": "1.2 Ynglŷn â'ch ased",
+            },
             "tree_path": "1037.1.1.2",
-            "form_name_json": {"en": "about-your-asset-25", "cy": ""},
+            "form_name_json": {
+                "en": "about-your-asset-25",
+                "cy": "ynglyn-ach-ased",
+            },
         },
         {
-            "section_name": {"en": "1.3 Your funding request", "cy": ""},
+            "section_name": {
+                "en": "1.3 Your funding request",
+                "cy": "1.3 Eich cais am gyllid",
+            },
             "tree_path": "1037.1.1.3",
-            "form_name_json": {"en": "your-funding-request-25", "cy": ""},
+            "form_name_json": {
+                "en": "your-funding-request-25",
+                "cy": "eich-cais-am-gyllid",
+            },
         },
         {
-            "section_name": {"en": "1.4 Development support provider (not scored)", "cy": ""},
+            "section_name": {
+                "en": "1.4 Development support provider (not scored)",
+                "cy": "1.4 Darparwr cymorth datblygu (heb ei sgorio)",
+            },
             "tree_path": "1037.1.1.4",
             "form_name_json": {"en": "development-support-provider-25", "cy": ""},
         },
         {
-            "section_name": {"en": "1.5 Declaration", "cy": ""},
+            "section_name": {
+                "en": "1.5 Declaration",
+                "cy": "1.5 Datganiad",
+            },
             "tree_path": "1037.1.1.5",
-            "form_name_json": {"en": "declaration-25", "cy": ""},
+            "form_name_json": {
+                "en": "declaration-25",
+                "cy": "datganiad",
+            },
         },
     ],
     "fund_config": {
         "id": "4db6072c-4657-458d-9f57-9ca59638317b",
         "short_name": "COF25-EOI",
         "welsh_available": True,
-        "owner_organisation_name": "None",
-        "owner_organisation_shortname": "None",
-        "owner_organisation_logo_uri": "None",
+        "owner_organisation_name": "Department for Levelling Up, Housing and Communities",
+        "owner_organisation_shortname": "DLUHC",
+        "owner_organisation_logo_uri": None,
         "funding_type": "EOI",
-        "name_json": {"en": "Community Ownership Fund 2025"},
-        "title_json": {"en": "expression of interest in applying for the Community Ownership Fund 2025"},
+        "name_json": {"en": "Community Ownership Fund 2025", "cy": "Y Cronfa Perchnogaeth Gymunedol 2025"},
+        "title_json": {
+            "en": "expression of interest in applying for the Community Ownership Fund 2025",
+            "cy": "Gwneud cais am ddatganiad o ddiddordeb mewn gwneud cais i'r Gronfa Perchnogaeth Gymunedol",
+        },
         "description_json": {
-            "en": "The Community Ownership Fund is a £150 million fund over 4 years to support community groups across England, Wales, Scotland and Northern Ireland to take ownership of assets which are at risk of being lost to the community."
+            "en": "The Community Ownership Fund is a £150 million fund over 4 years to support community groups across England, Wales, Scotland and Northern Ireland to take ownership of assets which are at risk of being lost to the community.",
+            "cy": (  # TODO: Provide welsh translation
+                "The Community Ownership Fund is a £150 million fund over 4 years"
+                " to support community groups across England, Wales, Scotland and"
+                " Northern Ireland to take ownership of assets which are at risk"
+                " of being lost to the community."
+            ),
         },
     },
     "round_config": {
@@ -59,13 +98,16 @@ LOADER_CONFIG={
         "privacy_notice": "https://www.gov.uk/government/publications/community-ownership-fund-privacy-notice/community-ownership-fund-privacy-notice",
         "reference_contact_page_over_email": True,
         "contact_email": "COF@communities.gov.uk",
-        "contact_phone": "0",
-        "contact_textphone": "0",
+        "contact_phone": None,
+        "contact_textphone": None,
         "support_times": "9am to 5pm",
         "support_days": "Monday to Friday",
         "instructions_json": {
             "en": "You must complete this Expression of Interest (EOI) form if you are interested in applying for the Community Ownership Fund 2025 (COF25).",
-            "cy": "",
+            "cy": (
+                "Mae'n rhaid i chi gwblhau'r ffurflen Datganiad o Ddiddordeb hon os"
+                " oes diddordeb gennych mewn gwneud cais i'r Gronfa Perchnogaeth Gymunedol 2025."
+            ),
         },
         "feedback_link": "https://www.gov.uk/government/organisations/ministry-of-housing-communities-local-government",
         "project_name_field_id": "SMRWjl",
@@ -81,13 +123,28 @@ LOADER_CONFIG={
             "has_feedback_survey": False,
             "has_section_feedback": True,
             "is_feedback_survey_optional": False,
-            "is_section_feedback_optional": False,
+            "is_section_feedback_optional": True,
         },
         "eligibility_config": {"has_eligibility": False},
-        "title_json": {"en": "Round 1"},
+        "title_json": {"en": "Round 1", "cy": "Rownd 1"},
         "contact_us_banner_json": {
-            "en": '<h2 class="govuk-heading-m">Get application support</h2>\r\n                 <p class="govuk-body">\r\n                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund">Visit the My Community website</a>\r\n                    for information and guidance on applying to Community Ownership Fund.\r\n                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund#enquiry-form">Fill out the enquiry form</a>\r\n                    to request advice from My Community.\r\n                </p>\r\n                <p class="govuk-body">\r\n                    We cannot provide direct support to applicants outside of this service.\r\n                </p>\r\n                <h2 class="govuk-heading-m">Get technical support</h2>\r\n                <p  class="govuk-body">\r\n                    Contact the Ministry of Housing, Communities & Local Government funding team if you need\r\n                    help with accessing or submitting an application form.\r\n                </p>',
-            "cy": "",
+            "en": '<h2 class="govuk-heading-m">Get application support</h2>\r\n                 <p class="govuk-body">\r\n                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund">Visit the My Community website</a>\r\n                    for information and guidance on applying to Community Ownership Fund 2025.\r\n                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund#enquiry-form">Fill out the enquiry form</a>\r\n                    to request advice from My Community.\r\n                </p>\r\n                <p class="govuk-body">\r\n                    We cannot provide direct support to applicants outside of this service.\r\n                </p>\r\n                <h2 class="govuk-heading-m">Get technical support</h2>\r\n                <p  class="govuk-body">\r\n                    Contact the Ministry of Housing, Communities & Local Government funding team if you need\r\n                    help with accessing or submitting an application form.\r\n                </p>',
+            "cy": """
+                 <h2 class="govuk-heading-m">Cael cymorth â'r cais</h2>
+                 <p class="govuk-body">
+                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund">Ewch i wefan My Community</a>
+                    i gael gwybodaeth ac arweiniad ar wneud cais i'r Gronfa Perchnogaeth Gymunedol 2025.
+                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund#enquiry-form">Llenwch y ffurflen ymholiad</a>
+                    i ofyn am gyngor gan My Community.
+                </p>
+                <p class="govuk-body">
+                    Ni allwn ddarparu cymorth uniongyrchol i ymgeiswyr tu hwnt i'r gwasanaeth hwn.
+                </p>
+                <h2 class="govuk-heading-m">Cael cymorth technegol</h2>
+                <p  class="govuk-body">
+                    Cysylltwch â thîm cyllid yr Adran Ffyniant Bro, Tai a Chymunedau os oes angen help arnoch i gael at ffurflen gais neu ei chyflwyno.
+                </p>
+            """,
         },
     },
     "base_path": 1037,
