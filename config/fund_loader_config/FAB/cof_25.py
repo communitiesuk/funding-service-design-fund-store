55318f8a0522f4d3f1c30f193fa89907e507a42c
srh-sloan <srh-sloan@users.noreply.github.com>
2024-10-23 11:04:29 +0100

    FS-4684 COF25 Config (#347)
    * adding cof25 config
    
    * removing spaces
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fs-4684 fixing cof25 config jsons
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update form name
    
    * fs-4684 adding welsh config
    
    * form names
    
    * dept name
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: srh-sloan <sarah.sloan@communities.gov.uk>

---
+++ config/fund_loader_config/FAB/cof_25.py
@@ -0,0 +1,321 @@
+LOADER_CONFIG = {
+    "sections_config": [
+        {
+            "section_name": {
+                "en": "1. About your organisation",
+                "cy": "1. Ynglŷn â'ch sefydliad",
+            },
+            "tree_path": "1036.1.1",
+            "requires_feedback": True,
+        },
+        {
+            "section_name": {
+                "en": "1.1 Organisation information",
+                "cy": "1.1 Gwybodaeth am y sefydliad",
+            },
+            "form_name_json": {
+                "en": "organisation-information-cof",
+                "cy": "gwybodaeth-am-y-sefydliad-cof",
+            },
+            "tree_path": "1036.1.1.1",
+        },
+        {
+            "section_name": {
+                "en": "1.2 Applicant information",
+                "cy": "1.2 Gwybodaeth am yr ymgeisydd",
+            },
+            "form_name_json": {
+                "en": "applicant-information-cof",
+                "cy": "gwybodaeth-am-yr-ymgeisydd-cof",
+            },
+            "tree_path": "1036.1.1.2",
+        },
+        {
+            "section_name": {
+                "en": "2. About your project",
+                "cy": "2. Ynglŷn â'ch prosiect",
+            },
+            "tree_path": "1036.1.2",
+            "requires_feedback": True,
+        },
+        {
+            "section_name": {
+                "en": "2.1 Project information",
+                "cy": "2.1 Gwybodaeth am y prosiect",
+            },
+            "form_name_json": {
+                "en": "project-information-cof-25",
+                "cy": "gwybodaeth-am-y-prosiect-cof-25",
+            },
+            "tree_path": "1036.1.2.1",
+        },
+        {
+            "section_name": {
+                "en": "2.2 Asset information",
+                "cy": "2.2 Gwybodaeth am yr ased",
+            },
+            "form_name_json": {
+                "en": "asset-information-cof",
+                "cy": "gwybodaeth-am-yr-ased-cof",
+            },
+            "tree_path": "1036.1.2.2",
+        },
+        {
+            "section_name": {"en": "3. Strategic case", "cy": "3. Achos strategol"},
+            "tree_path": "1036.1.3",
+            "requires_feedback": True,
+            "weighting": 53,
+        },
+        {
+            "section_name": {"en": "3.1 Community use/significance", "cy": "3.1 Defnydd/arwyddocâd cymunedol"},
+            "form_name_json": {
+                "en": "community-use-cof",
+                "cy": "defnydd-cymunedol-cof",
+            },
+            "tree_path": "1036.1.3.1",
+        },
+        {
+            "section_name": {
+                "en": "3.2 Community engagement",
+                "cy": "3.2 Ymgysylltu â'r gymuned",
+            },
+            "form_name_json": {
+                "en": "community-engagement-cof",
+                "cy": "ymgysylltiad-cymunedol-cof",
+            },
+            "tree_path": "1036.1.3.2",
+        },
+        {
+            "section_name": {"en": "3.3 Local support", "cy": "3.3 Cefnogaeth leol"},
+            "form_name_json": {
+                "en": "local-support-25",
+                "cy": "cefnogaeth-leol-cof-25",
+            },
+            "tree_path": "1036.1.3.3",
+        },
+        {
+            "section_name": {
+                "en": "3.4 Community benefits",
+                "cy": "3.4 Buddion cymunedol",
+            },
+            "form_name_json": {
+                "en": "community-benefits-cof",
+                "cy": "buddion-cymunedol-cof",
+            },
+            "tree_path": "1036.1.3.4",
+        },
+        {
+            "section_name": {
+                "en": "3.5 Environmental sustainability",
+                "cy": "3.5 Cynaliadwyedd amgylcheddol",
+            },
+            "form_name_json": {
+                "en": "environmental-sustainability-cof",
+                "cy": "cynaliadwyedd-amgylcheddol-cof",
+            },
+            "tree_path": "1036.1.3.5",
+        },
+        {
+            "section_name": {"en": "4. Management case", "cy": "4. Achos rheoli"},
+            "tree_path": "1036.1.4",
+            "weighting": 47,
+            "requires_feedback": True,
+        },
+        {
+            "section_name": {
+                "en": "4.1 Funding required",
+                "cy": "4.1 Cyllid sydd ei angen",
+            },
+            "form_name_json": {
+                "en": "funding-required-cof",
+                "cy": "cyllid-sydd-ei-angen-cof",
+            },
+            "tree_path": "1036.1.4.1",
+        },
+        {
+            "section_name": {"en": "4.2 Feasibility ", "cy": "4.2 Dichonoldeb"},
+            "form_name_json": {
+                "en": "feasibility-cof",
+                "cy": "dichonoldeb-cof",
+            },
+            "tree_path": "1036.1.4.2",
+        },
+        {
+            "section_name": {"en": "4.3 Risk", "cy": "4.3 Risg"},
+            "tree_path": "1036.1.4.3",
+            "form_name_json": {"en": "risk-cof", "cy": "risg-cof"},
+        },
+        {
+            "section_name": {"en": "4.4 Operational costs", "cy": "4.4 Costau gweithredol"},
+            "form_name_json": {
+                "en": "operational-costs-cof",
+                "cy": "costau-gweithredol-cof",
+            },
+            "tree_path": "1036.1.4.4",
+        },
+        {
+            "section_name": {
+                "en": "4.5 Skills and resources",
+                "cy": "4.5 Sgiliau ac adnoddau",
+            },
+            "form_name_json": {
+                "en": "skills-and-resources-cof",
+                "cy": "sgiliau-ac-adnoddau-cof",
+            },
+            "tree_path": "1036.1.4.5",
+        },
+        {
+            "section_name": {
+                "en": "4.6 Community representation",
+                "cy": "4.6 Cynrychiolaeth gymunedol",
+            },
+            "form_name_json": {
+                "en": "community-representation-cof",
+                "cy": "cynrychiolaeth-gymunedol-cof",
+            },
+            "tree_path": "1036.1.4.6",
+        },
+        {
+            "section_name": {
+                "en": "4.7 Inclusiveness and integration",
+                "cy": "4.7 Cynhwysiant ac integreiddio",
+            },
+            "form_name_json": {
+                "en": "inclusiveness-and-integration-cof",
+                "cy": "cynhwysiant-ac-integreiddio-cof",
+            },
+            "tree_path": "1036.1.4.7",
+        },
+        {
+            "section_name": {
+                "en": "4.8 Upload business plan",
+                "cy": "4.8 Lanlwythwch y cynllun busnes",
+            },
+            "form_name_json": {
+                "en": "upload-business-plan-cof-25",
+                "cy": "lanlwythwch-y-cynllun-busnes-cof-25",
+            },
+            "tree_path": "1036.1.4.8",
+        },
+        {
+            "section_name": {"en": "5. Check declarations", "cy": "5. Gwirio datganiadau"},
+            "tree_path": "1036.1.5",
+            "requires_feedback": None,
+        },
+        {
+            "section_name": {"en": "5.1 Declarations", "cy": "5.1 Datganiadau"},
+            "form_name_json": {
+                "en": "declarations-cof",
+                "cy": "cadarnhadau-terfynol-cof",
+            },
+            "tree_path": "1036.1.5.1",
+        },
+    ],
+    "fund_config": {
+        "id": "604450fe-65c0-4a2e-a4ba-30ccf256056b",
+        "short_name": "COF25",
+        "welsh_available": True,
+        "owner_organisation_name": "Ministry of Housing, Communities and Local Government",
+        "owner_organisation_shortname": "MHCLG",
+        "owner_organisation_logo_uri": "None",
+        "funding_type": "COMPETITIVE",
+        "name_json": {
+            "en": "Community Ownership Fund 2025",
+            "cy": "Y Cronfa Perchnogaeth Gymunedol 2025",
+        },
+        "title_json": {
+            "en": "funding to save an asset in your community",
+            "cy": "gyllid i achub ased yn eich cymuned",
+        },
+        "description_json": {
+            "en": "The Community Ownership Fund is a £150 million fund over 4 years to support community groups across England, Wales, Scotland and Northern Ireland to take ownership of assets which are at risk of being lost to the community.",
+            "cy": (  # TODO: Provide welsh translation
+                "The Community Ownership Fund is a £150 million fund over 4 years"
+                " to support community groups across England, Wales, Scotland and"
+                " Northern Ireland to take ownership of assets which are at risk"
+                " of being lost to the community."
+            ),
+        },
+    },
+    "round_config": {
+        "id": "38914fbf-9c31-41be-8547-c24d997aaba2",
+        "fund_id": "604450fe-65c0-4a2e-a4ba-30ccf256056b",
+        "short_name": "R1",
+        "opens": "2024-10-30T11:59:00",
+        "assessment_start": "2025-01-01T09:00:00",
+        "deadline": "2024-12-11T14:00:00",
+        "application_reminder_sent": False,
+        "reminder_date": "2024-12-01T11:59:00",
+        "assessment_deadline": "2025-01-31T17:00:00",
+        "prospectus": "https://www.gov.uk/government/publications/community-ownership-fund-prospectus/community-ownership-fund-2025-prospectus",
+        "privacy_notice": (
+            "https://www.gov.uk/government/publications/community-ownership-fund-"
+            "privacy-notice/community-ownership-fund-privacy-notice"
+        ),
+        "reference_contact_page_over_email": False,
+        "contact_email": "COF@communities.gov.uk",
+        "contact_phone": None,
+        "contact_textphone": None,
+        "support_times": "9am to 5pm",
+        "support_days": "Monday to Friday",
+        "instructions_json": {
+            "en": "You must have received an invitation to apply. If we did not invite you, first <a href='https://www.gov.uk/guidance/community-ownership-fund-round-4-how-to-express-your-interest-in-applying'>express your interest in the fund</a>.",
+            "cy": (
+                "Mae'n rhaid i chi fod wedi derbyn gwahoddiad i ymgeisio. Os na wnaethom eich gwahodd, <a"
+                ' href="https://www.gov.uk/guidance/community-ownership-fund-round-4-how-'
+                'to-express-your-interest-in-applying">'
+                " mynegwch eich diddordeb yn y gronfa yn gyntaf</a>."
+            ),
+        },
+        "feedback_link": (
+            "https://forms.office.com/Pages/ResponsePage.aspx?id="
+            "EGg0v32c3kOociSi7zmVqFJBHpeOL2tNnpiwpdL2iElURUY1WkhaS0NFMlZVQUhYQ1NaN0E4RjlQMC4u"
+        ),
+        "project_name_field_id": "apGjFS",
+        "application_guidance_json": {
+            "en": "<h2 class='govuk-heading govuk-heading-s'>What we'll ask you for</h2><p class='govuk-body'>You can preview the <a href='{all_questions_url}'>full list of application questions</a>.</p><p class='govuk-body'>We'll also ask you to upload a business plan to support the answers you've given us in the management case section.</p>",
+            "cy": (
+                "<h2 class='govuk-heading govuk-heading-s'>Beth fyddwn ni'n gofyn i chi amdano</h2><p"
+                " class='govuk-body'>Gallwch gael cip ymlaen llaw <a href='{all_questions_url}'>o restr lawn"
+                " cwestiynau'r cais</a>.</p><p class='govuk-body'>Byddwn hefyd yn gofyn i chi "
+                "lanlwytho cynllun busnes i ategu'r atebion rydych wedi'u rhoi i ni yn"
+                " yr adran achos rheoli.</p>"
+            ),
+        },
+        "guidance_url": "https://www.gov.uk/government/organisations/ministry-of-housing-communities-local-government",
+        "all_uploaded_documents_section_available": False,
+        "application_fields_download_available": True,
+        "display_logo_on_pdf_exports": False,
+        "mark_as_complete_enabled": True,
+        "is_expression_of_interest": False,
+        "eoi_decision_schema": None,
+        "feedback_survey_config": {
+            "has_feedback_survey": True,
+            "has_section_feedback": True,
+            "is_feedback_survey_optional": False,
+            "is_section_feedback_optional": False,
+        },
+        "eligibility_config": {"has_eligibility": False},
+        "title_json": {"en": "Round 1", "cy": "Rownd 1"},
+        "contact_us_banner_json": {
+            "en": '<h2 class="govuk-heading-m">Get application support</h2>\r\n                 <p class="govuk-body">\r\n                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund">Visit the My Community website</a>\r\n                    for information and guidance on applying to Community Ownership Fund 2025.\r\n                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund#enquiry-form">Fill out the enquiry form</a>\r\n                    to request advice from My Community.\r\n                </p>\r\n                <p class="govuk-body">\r\n                    We cannot provide direct support to applicants outside of this service.\r\n                </p>\r\n                <h2 class="govuk-heading-m">Get technical support</h2>\r\n                <p  class="govuk-body">\r\n                    Contact the Department of Levelling Up, Housing and Communities funding team if you need\r\n                    help with accessing or submitting an application form.\r\n                </p>',
+            "cy": """
+                 <h2 class="govuk-heading-m">Cael cymorth â'r cais</h2>
+                 <p class="govuk-body">
+                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund">Ewch i wefan My Community</a>
+                    i gael gwybodaeth ac arweiniad ar wneud cais i'r Gronfa Perchnogaeth Gymunedol 2025.
+                    <a class="govuk-link" href="https://mycommunity.org.uk/community-ownership-fund#enquiry-form">Llenwch y ffurflen ymholiad</a>
+                    i ofyn am gyngor gan My Community.
+                </p>
+                <p class="govuk-body">
+                    Ni allwn ddarparu cymorth uniongyrchol i ymgeiswyr tu hwnt i'r gwasanaeth hwn.
+                </p>
+                <h2 class="govuk-heading-m">Cael cymorth technegol</h2>
+                <p  class="govuk-body">
+                    Cysylltwch â thîm cyllid yr Adran Ffyniant Bro, Tai a Chymunedau os oes angen help arnoch i gael at ffurflen gais neu ei chyflwyno.
+                </p>
+            """,
+        },
+    },
+    "base_path": 1036,
+}
